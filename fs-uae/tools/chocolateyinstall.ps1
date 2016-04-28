7893e19d05bb36d3c3b5e1662d5fc0a3c6daa862
jtcmedia <jason.cumiskey@gmail.com>
2016-04-28 16:52:28 -0700

    initial commit

---
+++ fs-uae/tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'fs-uae'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://fs-uae.net/stable/2.6.2/fs-uae-suite_2.6.2_windows.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+  softwareName  = 'fs-uae*'
+  checksum      = '7d2b113108fa2b2c0898e7fc84733b5b'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
