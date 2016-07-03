5ae2dca20070996be1a633349c1570ac9d3d46cb
jtcmedia <jason.cumiskey@gmail.com>
2016-07-02 23:31:55 -0700

    added uninstall script to bitvise-ssh-client

---
+++ bitvise-ssh-client/tools/chocolateyuninstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'bitvise-ssh-client'
+$softwareName = "Bitvise SSH Client*"
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+$key | ForEach-Object {
+  Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                              -FileType "$installerType" `
+                              -SilentArgs "$($silentArgs)" `
+                              -File "& $($_.UninstallString.Replace('"',''))" `
+                              -ValidExitCodes $validExitCodes
+}
