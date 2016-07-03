72ba51064e3347da43de782b29c4e44e8b0db435
jtcmedia <jason.cumiskey@gmail.com>
2016-07-02 22:29:40 -0700

    added bitvise-ssh-client pkg w/version 7.12

---
+++ bitvise-ssh-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'bitvise-ssh-client'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://bvdl.s3-eu-west-1.amazonaws.com/BvSshClient-Inst.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs	= '-acceptEULA -force -noDesktopIcon'
+  validExitCodes= @(0)
+
+  softwareName  = 'Bitvise SSH Client*'
+  checksum      = '50f5d9d6db90a434082794707f7bd2f6'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
