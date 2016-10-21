d5a568200a8bc44a657904c21584e82cc7cf4c62
jtcmedia <jason.cumiskey@gmail.com>
2016-10-20 19:13:58 -0700

    add torguard client

---
+++ torguard-client/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'torguard-client'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://updates.torguard.biz/Software/Windows/torguard-setup-latest.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = '*TorGuard*'
+
+  checksum      = '3b75560e8f02a215ec1b6d64dd670b650735d1283193047c1cbbc0e67068c744'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $toolsDir "chocolateyinstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+Install-ChocolateyPackage @packageArgs
