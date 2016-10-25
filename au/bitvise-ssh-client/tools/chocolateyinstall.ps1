a77e827ec01971efc649bc81013ab65702bd1a3f
jtcmedia <jason.cumiskey@gmail.com>
2016-10-24 22:45:15 -0700

    added uninstall script for bitvise ssh client

--- au/bitvise-ssh-client/tools/chocolateyinstall.ps1
+++ au/bitvise-ssh-client/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName= 'bitvise-ssh-client'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://bvdl.s3-eu-west-1.amazonaws.com/BvSshClient-Inst.exe'
-$checksum32 = ''
+$checksum32 = 'c90d950ff4d6cc59bb923443a06e4d309a2d312cb470a41d42c6afd54ece79af'
 
 $packageArgs = @{
   packageName   = $packageName
---
+++ au/bitvise-ssh-client/tools/chocolateyuninstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'bitvise-ssh-client'
+$softwareName = 'Bitvise SSH Client*'
+$installerType = 'EXE' 
+
+$silentArgs = '-unat'
+$validExitCodes = @(1)
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+  $key[0] | % { 
+    $file = "$($_.UninstallString)"
+	
+	Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
