d6da5a63752f704847df39cf5f6939093db85f8b
Chocolatey <chocolatey@realdimensions.net>
2016-11-07 19:25:57 +0000

    AU:  updated - fs-uae [skip ci]

--- au/fs-uae/tools/chocolateyinstall.ps1
+++ au/fs-uae/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-﻿
+
 $ErrorActionPreference = 'Stop';
 
 
 $packageName= 'fs-uae'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://fs-uae.net/stable/2.8.0/fs-uae-suite_2.8.0_windows_x86.exe'
-$url64      = 'https://fs-uae.net/stable/2.8.0/fs-uae-suite_2.8.0_windows_x86-64.exe'
-$checksum32 = '96293a9efb030ed8f79db56c9e2e1b0fa3889ce2d61c50878d38ba00cc1bad6b'
-$checksum64 = '33f6848cce6449651628d58a6a6c3e4d407ed90aa3eacd2fce22a520f0187c21'
+$url32      = 'https://fs-uae.net/stable/2.8.1u3/fs-uae-suite_2.8.1u3_windows_x86.exe'
+$url64      = 'https://fs-uae.net/stable/2.8.1u3/fs-uae-suite_2.8.1u3_windows_x86-64.exe'
+$checksum32 = 'da1894d23c76d686556046b56f4d89517b6d996ec2ecdbc084fe5f5e033e0190'
+$checksum64 = '35c55d212d93f5bd3e0f1afdbd85550ef9d95ab478e5c8898820d9e3d87df426'
 
 $packageArgs = @{
   packageName   = $packageName
