9163c045e39ab81954ad9f10d08fc83cdf14ce8a
jtcmedia <jason.cumiskey@gmail.com>
2016-11-14 10:48:12 -0800

    fixes to update script for torguard, rename phpstorm icon

--- au/torguard-client/update.ps1
+++ au/torguard-client/update.ps1
@@ -13,11 +13,11 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     	
-	if ($download_page.Content -match 'v0.\d.\d{2}')
+	if ($download_page.Content -match 'v0\.\d\.\d{2}')
     {
         $version = $Matches[0].Substring(1)
     }
-    else { throw "Can't match version 'v0.\d.\d{2}'" }
+    else { throw "Can't match version 'v0\.\d\.\d{2}'" }
 
     return @{ Version = $version }
 }
