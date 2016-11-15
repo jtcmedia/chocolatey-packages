c2f465d8312845c52eef044fd47f507348c415ae
jtcmedia <jason.cumiskey@gmail.com>
2016-11-15 09:53:42 -0800

    remove tabs

--- au/fs-uae/update.ps1
+++ au/fs-uae/update.ps1
@@ -16,18 +16,18 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     
-	$regex = '.exe$'
-	$url = $download_page.links | ? href -match $regex | select -First 2 -expand href
-	
-	$version = $url[0] -split '_' | select -Index 1
-	# the author sometimes amends, for example, 2.8.1u3
-	# for stable releases. Change to 2.8.1.3
-	$version = $version -replace '[u]', '.'
-	
-	$url32 = 'https://fs-uae.net' + $url[0]
+    $regex = '.exe$'
+    $url = $download_page.links | ? href -match $regex | select -First 2 -expand href
+    
+    $version = $url[0] -split '_' | select -Index 1
+    # the author sometimes amends, for example, 2.8.1u3
+    # for stable releases. Change to 2.8.1.3
+    $version = $version -replace '[u]', '.'
+    
+    $url32 = 'https://fs-uae.net' + $url[0]
     $url64 = 'https://fs-uae.net' + $url[1]
-	
-	$Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
 }
 
