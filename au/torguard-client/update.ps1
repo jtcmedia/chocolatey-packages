a0e5e78e69f07173ad28191085c47a5cf36013ae
jtcmedia <jason.cumiskey@gmail.com>
2016-11-18 10:53:02 -0800

    add url check to update

--- au/torguard-client/update.ps1
+++ au/torguard-client/update.ps1
@@ -5,18 +5,21 @@ $releases = 'https://torguard.net/downloads.php'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+			"(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
-        
-    $version = ($download_page.ParsedHtml.getElementsByTagName("span") | ? ClassName -eq "latestver" | select -First 1).InnerText.Substring(1)
     
-    return @{ Version = $version }
+	$url = ($download_page.InputFields | select -First 1).outerHTML -match "'.*'" | Foreach {$Matches[0]}
+		
+	$version = ($download_page.ParsedHtml.getElementsByTagName("span") | ? ClassName -eq "latestver" | select -First 1).InnerText.Substring(1)
+    
+    return @{ URL32 = $url; Version = $version }
 
 }
 
-update -NoCheckUrl -ChecksumFor 32
+update -ChecksumFor 32
