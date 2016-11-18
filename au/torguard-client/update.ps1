12cd608fd3bcc1a726e4e0b967a5fdead08cdb4e
jtcmedia <jason.cumiskey@gmail.com>
2016-11-18 02:06:10 -0800

    fix to finding version number

--- au/torguard-client/update.ps1
+++ au/torguard-client/update.ps1
@@ -12,13 +12,9 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
-    	
-	if ($download_page.Content -match 'v0\.\d\.\d{2}')
-    {
-        $version = $Matches[0].Substring(1)
-    }
-    else { throw "Can't match version 'v0\.\d\.\d{2}'" }
-
+        
+    $version = ($download_page.ParsedHtml.getElementsByTagName("span") | ? ClassName -eq "latestver" | select -First 1).InnerText.Substring(1)
+    
     return @{ Version = $version }
 }
 
