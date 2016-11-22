30bb12c44f0844392c6545b7380b19cd204fd339
jtcmedia <jason.cumiskey@gmail.com>
2016-11-21 22:26:21 -0800

    add gnupg-modern package

---
+++ gnupg-modern/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'gnupg-modern'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.gnupg.org/ftp/gcrypt/binary/gnupg-w32-2.1.16_20161118.exe'
+$checksum32 = '14b17454b32f93099133056833b17dafc76946560be4439fb1894d5dc52fd9dc'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'GNU Privacy Guard*'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
