60ba9971926baa0dfb13eb8188a78e7ff5ffced4
jtcmedia <jason.cumiskey@gmail.com>
2016-12-14 12:25:28 -0800

    get-remotechecksum didn't work, change to au_beforeupdate

--- torguard-client/update.ps1
+++ torguard-client/update.ps1
@@ -1,18 +1,34 @@
 import-module au
 
 $releases = 'https://torguard.net/downloads.php'
-$url = 'http://updates.torguard.biz/Software/Windows/torguard-setup-latest.exe'
+$checksums = 'http://updates.torguard.biz/Software/Windows/checksums.sha256'
 
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
         ".\tools\VERIFICATION.txt" = @{
-            "(?i)(\s+checksum32:).*"    = "`${1} $($Latest.Checksum)"
+            "(?i)(\s+checksum32:).*"    = "`${1} $($Latest.Checksum32)"
         }
     }
+}
+
+
+function global:au_BeforeUpdate {
+      
+    $toolsPath = "$PSScriptRoot\tools"
+
+    $client = New-Object System.Net.WebClient
+        $fn = $checksums -split '/' | select -Last 1
+        Write-Host 'Getting checksum...'
+        $client.DownloadFile($checksums, "$toolsPath\$fn")
+        $Latest.Checksum32 = Get-Content "$toolsPath\$fn" | select -Last 1 | % { $_ -split '  ' | select -First 1 }
+    $client.Dispose()
+    
+    #don't need checksums file anymore
+    rm "$toolsPath\$fn"
 }
 
 
@@ -21,8 +37,8 @@ function global:au_GetLatest {
     
     $version = ($download_page.ParsedHtml.getElementsByTagName("span") | ? ClassName -eq "latestver" | select -First 1).InnerText.Substring(1)
     
-    return @{ Version = $version; Checksum = Get-RemoteChecksum $url }
+    return @{ Version = $version }
 
 }
 
-update -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
+update -NoCheckUrl -ChecksumFor none
