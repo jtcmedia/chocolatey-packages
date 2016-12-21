d394a32efe3092c104fb1ba21661ff4ad64040e3
Chocolatey <chocolatey@realdimensions.net>
2016-12-21 10:14:43 +0000

    AU: 1 updated - heroku-cli [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/b89f424988015b9aaf95fe4a4fcd623d47b17a87

--- heroku-cli/tools/chocolateyinstall.ps1
+++ heroku-cli/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $packageName = 'heroku-cli'
 
 #First need to uninstall old toolbelt pkg if installed
@@ -26,8 +26,8 @@ Remove-Item $env:USERPROFILE\.config\heroku -Recurse -ErrorAction SilentlyContin
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32      = 'https://cli-assets.heroku.com/branches/stable/heroku-windows-386.exe'
 $url64      = 'https://cli-assets.heroku.com/branches/stable/heroku-windows-amd64.exe'
-$checksum32 = 'ff234e8d966111a65d4049ea0fd77d4b433bb568d2816b5c8d58d87fd97c83c9'
-$checksum64 = 'ac7aef7f77c947f0b75022156d7cc5bfebdec1e258ffdcf641de1dcd9c5f5224'
+$checksum32 = '8e533dca6b3516626da5147ce46ccc2f61aac65e44c0df6fc8251336e4f39861'
+$checksum64 = 'dc84d9d737e764748a9175745e56cee4ee3ada9a25e0b83d452d014f656489db'
 
 $packageArgs = @{
   packageName   = $packageName
