bd3f3838338a7c05c598b8adc4474249e342f3b6
jtcmedia <jason.cumiskey@gmail.com>
2016-12-22 21:36:58 -0800

    updt to use Get-RemoteFiles; add legal folder

--- gmvault/update.ps1
+++ gmvault/update.ps1
@@ -4,37 +4,27 @@ $releases = 'http://gmvault.org/download.html'
 
 function global:au_SearchReplace {
     @{
-        ".\tools\VERIFICATION.txt" = @{
+        ".\legal\VERIFICATION.txt" = @{
             "(?i)(\s+x32:).*"           = "`${1} $($Latest.URL32)"
             "(?i)(\s+checksum32:).*"    = "`${1} $($Latest.Checksum32)"
+            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
         }
     }
 }
 
 
-function global:au_BeforeUpdate {
-    $toolsPath = "$PSScriptRoot\tools"
-
-    rm "$toolsPath\*.exe" -force -ea stop
-    $client = New-Object System.Net.WebClient
-        $fn = $Latest.URL32 -split '/' | select -Last 1
-        Write-Host 'Downloading installer: ' $fn
-        $client.DownloadFile($Latest.URL32, "$toolsPath\$fn")
-        $Latest.Checksum32 = Get-FileHash -Algorithm SHA256 -Path "$toolsPath\$fn" | % Hash
-    $client.Dispose()
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
     $regex = 'exe$'
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
         
     $version = ($url -split '_|.exe' | select -Last 1 -Skip 1).Substring(1)
     
-    $Latest = @{ URL32 = $url; Version = $version }
-    return $Latest
+    return @{ URL32 = $url; Version = $version }
 }
 
 update -ChecksumFor none
