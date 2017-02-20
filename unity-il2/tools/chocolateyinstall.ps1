76c5c62e59b0c5d690ae00f7f64fef1bb58c69bc
Peter Mounce <pete@neverrunwithscissors.com>
2017-02-20 21:03:44 +0000

    Add unity-il2 package (#11)
    * Add unity-il2 package
    
    * Update chocolateyinstall.ps1
    
    change pkg name to be consistent with others

---
+++ unity-il2/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-il2cpp'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/TargetSupportInstaller/UnitySetup-UWP-IL2CPP-Support-for-Editor-5.5.1f1.exe'
+$checksum       = '54752213d70a447a753b5c50b5ce4ade82b2a04b0600302010973fadea863b9e'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
