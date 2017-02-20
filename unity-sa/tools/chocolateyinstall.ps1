70a4592aa4f416f5801fe58ce346f8b25328ecc8
Peter Mounce <pete@neverrunwithscissors.com>
2017-02-20 20:56:59 +0000

    Add unity-sa package (#9)

---
+++ unity-sa/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-sa'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/WindowsStandardAssetsInstaller/UnityStandardAssetsSetup-5.5.1f1.exe'
+$checksum       = '4840a961dc62f8c7aced1753049a2e12e56c82f6d6e850413743710f10e66a52'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
