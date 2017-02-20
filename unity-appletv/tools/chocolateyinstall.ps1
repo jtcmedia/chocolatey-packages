843f120fab24dac9e435286eaa7b5d6894d50e7f
Peter Mounce <petermounce@improbable.io>
2017-02-20 14:06:46 +0000

    Add unity-appletv package

---
+++ unity-appletv/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-appletv'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-5.5.1f1.exe'
+$checksum       = 'a84cde7fab0f96f909a89fd59ddced103b40ad42cebc836916355a3afdf15718'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
