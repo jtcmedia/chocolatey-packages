6bbaf42a8c75f8163b57cd4283e548d7ef5eba9d
jtcmedia <jtcmedia@users.noreply.github.com>
2017-02-20 13:03:23 -0800

    Update chocolateyinstall.ps1
    change pkg name to be consistent with others

--- unity-il2/tools/chocolateyinstall.ps1
+++ unity-il2/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'unity-il2'
+$packageName    = 'unity-il2cpp'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/TargetSupportInstaller/UnitySetup-UWP-IL2CPP-Support-for-Editor-5.5.1f1.exe'
 $checksum       = '54752213d70a447a753b5c50b5ce4ade82b2a04b0600302010973fadea863b9e'
