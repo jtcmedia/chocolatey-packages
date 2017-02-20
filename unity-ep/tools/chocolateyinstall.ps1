060cb28098776c39b06cac30a64c2938b259c9f7
Peter Mounce <petermounce@improbable.io>
2017-02-20 14:09:14 +0000

    Add unity-ep package

---
+++ unity-ep/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-ep'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/WindowsExampleProjectInstaller/UnityExampleProjectSetup-5.5.1f1.exe'
+$checksum       = 'c95f1767386d4b33064d496aeb917689e5cb46b6a56b09b14edba7035259303e'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
