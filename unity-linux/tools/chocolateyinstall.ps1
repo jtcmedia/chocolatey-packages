90179855af643357b9897df5bed02a6b1b7f163a
Peter Mounce <petermounce@improbable.io>
2017-02-20 14:07:55 +0000

    Add unity-linux package

---
+++ unity-linux/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-linux'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/TargetSupportInstaller/UnitySetup-Linux-Support-for-Editor-5.5.1f1.exe'
+$checksum       = 'c87c85b1affdd202364df57dba3afcbfacfa8c8c6244e52afe7d1002776f7e79'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
