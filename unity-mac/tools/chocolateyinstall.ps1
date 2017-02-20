85996d9382957bc5c3b69649a676fda4d17cc420
Peter Mounce <pete@neverrunwithscissors.com>
2017-02-20 21:01:08 +0000

    Add unity-mac package (#14)

---
+++ unity-mac/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-mac'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/TargetSupportInstaller/UnitySetup-Mac-Support-for-Editor-5.5.1f1.exe'
+$checksum       = '11cac3cebe3c10a3ed037296709587065b202f0403b980892192228b65ce3ad0'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
