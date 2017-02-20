e4134d3337a17e511ebcf9a17d3ff0436d6ab8a9
Peter Mounce <pete@neverrunwithscissors.com>
2017-02-20 16:35:09 +0000

    Add unity-webgl package (#7)

---
+++ unity-webgl/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-webgl'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download.unity3d.com/download_unity/88d00a7498cd/TargetSupportInstaller/UnitySetup-WebGL-Support-for-Editor-5.5.1f1.exe'
+$checksum       = 'c32941c480f58d3c013a463440277c0c5529778d2e1ccf815ecd6867d7356b62'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
