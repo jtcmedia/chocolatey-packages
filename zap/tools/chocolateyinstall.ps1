36ab6985e7ac554979f669d95c2ac1a619179d53
Jason Cumiskey <jason.cumiskey@gmail.com>
2017-03-29 08:19:44 -0700

    fix url 32-bit error

--- zap/tools/chocolateyinstall.ps1
+++ zap/tools/chocolateyinstall.ps1
@@ -2,16 +2,16 @@
 
 $packageName    = 'zap'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://github.com/zaproxy/zaproxy/releases/download/2.5.0/ZAP_2.5.0_Windows.exe'
-$url64          = ''
-$checksum32     = '4ac72657e88733a77d5686274a72dd62ce35fb196faa7f5775ee998277aa1564'
+$url32          = 'https://github.com/zaproxy/zaproxy/releases/download/2.6.0/ZAP_2_6_0_windows-x32.exe'
+$url64          = 'https://github.com/zaproxy/zaproxy/releases/download/2.6.0/ZAP_2_6_0_windows.exe'
+$checksum32     = ''
 $checksum64     = ''
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
+  url           = $url32
   url64bit      = $url64
   softwareName  = 'OWASP ZAP*'
   checksum      = $checksum32
