3d00a8c002548990f0386e10b03a6c1e5123ae30
Jason Cumiskey <jason.cumiskey@gmail.com>
2017-03-29 12:56:23 -0700

    back to legacy webclient code

--- heroku-cli/update.ps1
+++ heroku-cli/update.ps1
@@ -1,5 +1,4 @@
 import-module AU
-import-module $Env:ChocolateyInstall\extensions\chocolatey-core\*.psm1
 
 $releases  = 'https://devcenter.heroku.com/articles/heroku-cli'
 $changelog = 'https://raw.githubusercontent.com/heroku/cli/master/CHANGELOG'
@@ -27,8 +26,16 @@ function global:au_GetLatest {
     $regex = '.exe$'
     $url = $download_page.links | ? href -match $regex | select -First 2 -expand href
         
-    $cl = Get-WebContent $changelog 
-    $version = $cl | select -First 1 | % { $_ -split '\s+' | select -First 1 }
+    $toolsPath = "$PSScriptRoot\tools"
+    
+    $client = New-Object System.Net.WebClient
+        $fn = $changelog -split '/' | select -Last 1
+        $client.DownloadFile($changelog, "$toolsPath\$fn")
+        $version = Get-Content "$toolsPath\$fn" | select -First 1 | % { $_ -split '\s+' | select -First 1 }
+    $client.Dispose()
+    
+    #don't need changelog file anymore
+    Remove-Item "$toolsPath\$fn"
     
     return @{ URL32 = $url[0]; URL64 = $url[1]; Version = $version }
 }
