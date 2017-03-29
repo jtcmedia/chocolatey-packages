8c674f996b35f3437bcabc0ef373925f2137f2ac
Jason Cumiskey <jason.cumiskey@gmail.com>
2017-03-29 12:17:50 -0700

    zap changed from inno to install4j

--- zap/tools/chocolateyinstall.ps1
+++ zap/tools/chocolateyinstall.ps1
@@ -8,18 +8,18 @@ $checksum32     = '328533a23c24202c0e945e437d13dcf3cd83f5d20472d1a40e844deb32b39
 $checksum64     = '8b3ce9a4b1e93994a75595f3658719459390c04ea49fd1d3434066c2b047180f'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url32
-  url64bit      = $url64
-  softwareName  = 'OWASP ZAP*'
-  checksum      = $checksum32
-  checksumType  = 'sha256'
-  checksum64    = $checksum64
-  checksumType64= 'sha256'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url32
+  url64bit       = $url64
+  softwareName   = 'OWASP ZAP*'
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '-q'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
