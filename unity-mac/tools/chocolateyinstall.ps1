292edbb1e60ff2154a5f4f1d949c9c06a0e10995
jtcmedia <jtcmedia@users.noreply.github.com>
2017-06-23 00:16:41 -0700

    updt -> 5.6.2

--- unity-mac/tools/chocolateyinstall.ps1
+++ unity-mac/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-mac'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://download.unity3d.com/download_unity/2860b30f0b54/TargetSupportInstaller/UnitySetup-Mac-Support-for-Editor-5.6.1f1.exe'
-$checksum       = 'b6c2fc4bfcb6f7e6463c20f61f68ea7957c4d472d7990e35a0791e557697d9e3'
+$url            = 'https://download.unity3d.com/download_unity/a2913c821e27/TargetSupportInstaller/UnitySetup-Mac-Support-for-Editor-5.6.2f1.exe'
+$checksum       = 'ba769ba4f5bfdb6b1ad38b5ef5a5724869fd064a2c65a62c623c9e24adf05abb'
 
 $packageArgs = @{
   packageName    = $packageName
--- unity-webgl/tools/chocolateyinstall.ps1
+++ unity-webgl/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-webgl'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://download.unity3d.com/download_unity/497a0f351392/TargetSupportInstaller/UnitySetup-WebGL-Support-for-Editor-5.6.0f3.exe'
-$checksum       = '165c4321e3b5f0a1e2281ef487f49b185aa0b2ad1f7505faf8a6e18be8add61a'
+$url            = 'https://download.unity3d.com/download_unity/a2913c821e27/TargetSupportInstaller/UnitySetup-WebGL-Support-for-Editor-5.6.2f1.exe'
+$checksum       = '673c5e94d274e55439c7ce83604d1388be894a40b6520e83a642c5b20afd6698'
 
 $packageArgs = @{
   packageName    = $packageName
