5a46648e019fb3211968ab1a11175f744ec3cb0c
jtcmedia <jtcmedia@users.noreply.github.com>
2017-06-27 11:19:03 -0700

    add ds4windows

---
+++ ds4windows/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName  = $env:ChocolateyPackageName
+$packageTitle = $env:ChocolateyPackageTitle
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipFile      = Get-Item "$toolsDir\*_x64.zip"
+
+Get-ChocolateyUnzip -FileFullPath64 $zipFile `
+    -PackageName $packageName `
+    -Destination $toolsDir
+
+$startupPath = [Environment]::GetFolderPath("Startup")
+$startMenuPath = [Environment]::GetFolderPath("CommonPrograms")
+
+if(Test-Path -Path "$startupPath\$packageName.lnk") {
+    Install-ChocolateyShortcut `
+        -ShortcutFilePath "$startupPath\$packageName.lnk" `
+        -TargetPath "$env:ChocolateyInstall\bin\$packageTitle.exe"
+}
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath "$startMenuPath\$packageName.lnk" `
+  -TargetPath "$env:ChocolateyInstall\bin\$packageTitle.exe"
+
+# don't need installer zip archive anymore; delete old files if exist
+Remove-Item $toolsDir\* -Exclude $packageTitle, *.ps1, *.txt -ErrorAction SilentlyContinue -Force
---
+++ ds4windows/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+$releases = 'https://github.com/Ryochan7/DS4Windows/releases'
+
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\VERIFICATION.txt" = @{
+            "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+            "(?i)(\s+checksum64:).*"     = "`${1} $($Latest.Checksum64)"
+            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+        }
+    }
+}
+
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = '.zip$'
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+    
+    $version = $url -split '_' | select -Last 1 -Skip 1
+    
+    $url64 = 'https://github.com' + $url
+    
+    return @{ URL64 = $url64; Version = $version }
+}
+
+update -ChecksumFor none
