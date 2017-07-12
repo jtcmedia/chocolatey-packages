0387ce3e2300aa1e6081b21e0861491ed1d5d57c
jtcmedia <jtcmedia@users.noreply.github.com>
2017-07-12 14:38:21 -0700

    add new unity facebook target support

---
+++ unity-facebook/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-facebook'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64          = 'https://download.unity3d.com/download_unity/a2913c821e27/TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-5.6.2f1.exe'
+$checksum64     = '27760f90b12bb6b5d1ecaf82d648b174d1ce46f7f1c21edefdbb6f5480d5698f'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ unity-facebook/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+#$releases = 'https://unity3d.com/get-unity/update'
+$releases = 'https://unity3d.com/unity/whats-new/'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+        }
+        
+        ".\unity-facebook.nuspec" = @{
+            "(\<dependency id=`"unity`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+        }
+    }
+}
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = '.exe$'
+    $url = $download_page.links | ? href -match $regex | select -First 1 -Skip 6 -expand href
+    
+    $version = $url -split '-|f' | select -Last 1 -Skip 1
+    
+    return @{ URL64 = $url -replace 'http', 'https'; Version = $version }
+}
+
+update -ChecksumFor 64
