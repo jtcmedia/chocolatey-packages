4e3e73d5ca27011d57b9de5005c1a0b1c79d3271
jtcmedia <jtcmedia@users.noreply.github.com>
2017-09-05 01:42:56 -0700

    change release paths; needs a better way to do this

--- unity-standard-assets/update.ps1
+++ unity-standard-assets/update.ps1
@@ -1,7 +1,8 @@
 import-module au
 
 #$releases = 'https://unity3d.com/get-unity/update'
-$releases = 'https://unity3d.com/unity/whats-new/'
+#$releases = 'https://unity3d.com/unity/whats-new/'
+$releases = 'https://unity3d.com/get-unity/download/archive'
 
 function global:au_SearchReplace {
     @{
@@ -21,11 +22,12 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
     $regex = '.exe$'
-    $url = $download_page.links | ? href -match $regex | select -Last 1 -Skip 2 -expand href
+    #$url = $download_page.links | ? href -match $regex | select -Last 1 -Skip 2 -expand href
+    $url = $download_page.links | ? href -match $regex | select -First 1 -Skip 1 -expand href
     
     $version = $url -split '-|f' | select -Last 1 -Skip 1
     
-    return @{ URL64 = $url -replace 'http', 'https'; Version = $version }
+    return @{ URL64 = $url -replace 'http:', 'https:'; Version = $version }
 }
 
 update -ChecksumFor 64
--- unity/update.ps1
+++ unity/update.ps1
@@ -1,7 +1,8 @@
 import-module au
 
 #$releases = 'https://unity3d.com/get-unity/update'
-$releases = 'https://unity3d.com/unity/whats-new/'
+#$releases = 'https://unity3d.com/unity/whats-new/'
+$releases = 'https://unity3d.com/get-unity/download/archive'
 
 function global:au_SearchReplace {
     @{
@@ -20,7 +21,7 @@ function global:au_GetLatest {
     
     $version = $url -split '-|f' | select -Last 1 -Skip 1
     
-    return @{ URL64 = $url -replace 'http', 'https'; Version = $version }
+    return @{ URL64 = $url -replace 'http:', 'https:'; Version = $version }
 }
 
 update -ChecksumFor 64
