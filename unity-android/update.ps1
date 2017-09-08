ed4cbf3e145e2598eda0b3a3a5fc429765f05239
jtcmedia <jtcmedia@users.noreply.github.com>
2017-09-08 14:59:57 -0700

    uses xml file written by unity updt script instead

--- unity-android/update.ps1
+++ unity-android/update.ps1
@@ -1,7 +1,6 @@
 import-module au
 
-#$releases = 'https://unity3d.com/get-unity/update'
-$releases = 'https://unity3d.com/unity/whats-new/'
+$unity_data = Import-CliXml $PSScriptRoot\..\_unity.xml
 
 function global:au_SearchReplace {
     @{
@@ -18,14 +17,11 @@ function global:au_SearchReplace {
 
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  
+    $url = $unity_data["url"] + "TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-" + $unity_data["version"] + "f" + $unity_data["release"]
     
-    $regex = '.exe$'
-    $url = $download_page.links | ? href -match $regex | select -First 1 -Skip 3 -expand href
     
-    $version = $url -split '-|f' | select -Last 1 -Skip 1
-    
-    return @{ URL64 = $url -replace 'http', 'https'; Version = $version }
+    return @{ URL64 = $url -replace 'http:', 'https:'; Version = $unity_data["version"] }
 }
 
 update -ChecksumFor 64
