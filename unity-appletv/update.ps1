9eb9f979e787e8a9de431437374554b2ca9e66fb
jtcmedia <jtcmedia@users.noreply.github.com>
2017-09-08 15:21:50 -0700

    use xml file for updt

--- unity-appletv/update.ps1
+++ unity-appletv/update.ps1
@@ -1,7 +1,6 @@
 import-module au
 
-#$releases = 'https://unity3d.com/get-unity/update'
-$releases = 'https://unity3d.com/unity/whats-new/'
+$unity_data = Import-CliXml $PSScriptRoot\..\_unity.xml
 
 function global:au_SearchReplace {
     @{
@@ -10,7 +9,7 @@ function global:au_SearchReplace {
             "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
         }
         
-        ".\unity-appletv.nuspec" = @{
+        ".\unity-android.nuspec" = @{
             "(\<dependency id=`"unity`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
@@ -18,14 +17,11 @@ function global:au_SearchReplace {
 
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  
+    $url = $unity_data["url"] + "TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-" + $unity_data["version"] + "f" + $unity_data["release"]
     
-    $regex = '.exe$'
-    $url = $download_page.links | ? href -match $regex | select -First 1 -Skip 5 -expand href
     
-    $version = $url -split '-|f' | select -Last 1 -Skip 1
-    
-    return @{ URL64 = $url -replace 'http', 'https'; Version = $version }
+    return @{ URL64 = $url -replace 'http:', 'https:'; Version = $unity_data["version"] }
 }
 
 update -ChecksumFor 64
