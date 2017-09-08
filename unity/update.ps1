eefaa6013242eb4bcaf29980b1956105f7e096e0
jtcmedia <jtcmedia@users.noreply.github.com>
2017-09-08 14:58:51 -0700

    unity updt script writes xml file now; add to gitignore

--- unity/update.ps1
+++ unity/update.ps1
@@ -20,6 +20,17 @@ function global:au_GetLatest {
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
     
     $version = $url -split '-|f' | select -Last 1 -Skip 1
+    $release = $url -split 'f' | select -Last 1
+    $url_start = $url -split 'W' | select -First 1
+    
+    $unity_data = @{}
+    $unity_data["version"] = "$($version)"
+    $unity_data["url"] = "$($url_start)"
+    $unity_data["release"] = "$($release)"
+    
+    $file = "$PSScriptRoot\..\_unity.xml"
+    
+    $unity_data | Export-CliXml $PSScriptRoot\..\_unity.xml
     
     return @{ URL64 = $url -replace 'http:', 'https:'; Version = $version }
 }
