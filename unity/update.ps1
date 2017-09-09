d0eda5a1687a285f628f8690269cceed0ead9dfd
jtcmedia <jtcmedia@users.noreply.github.com>
2017-09-08 23:27:26 -0700

    take into account major/minor releases. Closes #25.

--- unity/update.ps1
+++ unity/update.ps1
@@ -1,8 +1,8 @@
 import-module au
 
 #$releases = 'https://unity3d.com/get-unity/update'
-#$releases = 'https://unity3d.com/unity/whats-new/'
-$releases = 'https://unity3d.com/get-unity/download/archive'
+$major_releases = 'https://unity3d.com/unity/whats-new'
+$minor_releases = 'https://unity3d.com/get-unity/download/archive'
 
 function global:au_SearchReplace {
     @{
@@ -14,10 +14,16 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page = Invoke-WebRequest -Uri $major_releases -UseBasicParsing
     
     $regex = '.exe$'
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+    
+    if ($url -eq $null) {
+        # it's a minor release
+        $download_page = Invoke-WebRequest -Uri $minor_releases -UseBasicParsing
+        $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+    } 
     
     $version = $url -split '-|f' | select -Last 1 -Skip 1
     $release = $url -split 'f' | select -Last 1
@@ -27,8 +33,6 @@ function global:au_GetLatest {
     $unity_data["version"] = "$($version)"
     $unity_data["url"] = "$($url_start)"
     $unity_data["release"] = "$($release)"
-    
-    $file = "$PSScriptRoot\..\_unity.xml"
     
     $unity_data | Export-CliXml $PSScriptRoot\..\_unity.xml
     
