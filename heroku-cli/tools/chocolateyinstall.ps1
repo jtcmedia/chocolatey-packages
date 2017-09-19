626ce2da74226b95d5f5ba1560285d3f6e48f20c
jtcmedia <jason.cumiskey@gmail.com>
2017-09-19 16:42:12 -0700

    add update script for heroku; exe's have been renamed, fix in install script

--- heroku-cli/tools/chocolateyinstall.ps1
+++ heroku-cli/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $packageName = 'heroku-cli'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing x64 bit version"; Get-Item "$toolsDir\*amd64.exe"
-} else { Write-Host "Installing x32 bit version"; Get-Item "$toolsDir\*386.exe" }
+         Write-Host "Installing x64 bit version"; Get-Item "$toolsDir\*x64.exe"
+} else { Write-Host "Installing x32 bit version"; Get-Item "$toolsDir\*x86.exe" }
 
 $packageArgs = @{
   packageName   = $packageName
---
+++ heroku-cli/update.ps1
@@ -0,0 +1,38 @@
+import-module AU
+
+$releases  = 'https://devcenter.heroku.com/articles/heroku-cli'
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+            "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+            "(?i)(\s+checksum32:).*"     = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+checksum64:).*"     = "`${1} $($Latest.Checksum64)"
+            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+        }
+    }
+}
+
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+
+function global:au_GetLatest {
+    $outFile    = "heroku-64bit-installer.exe"
+    
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex = '.exe$'
+    $url = $download_page.links | ? href -match $regex | select -First 2 -expand href
+    
+    Invoke-WebRequest $url[1] -OutFile "$PSScriptRoot\$outFile"
+    
+    $version = (Get-Item $outFile).VersionInfo.ProductVersion
+    
+    #don't need installer anymore
+    Remove-Item $outFile -Force
+    
+    return @{ URL32 = $url[0]; URL64 = $url[1]; Version = $version }
+}
+
+update -ChecksumFor none
