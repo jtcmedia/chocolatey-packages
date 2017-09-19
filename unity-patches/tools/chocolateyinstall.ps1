d78099dfed35db12dbfcde69f94620f09d9b9c68
jtcmedia <jason.cumiskey@gmail.com>
2017-09-19 16:43:36 -0700

    updt unity to patch 2

--- unity-patches/tools/chocolateyinstall.ps1
+++ unity-patches/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName        = 'unity'
 $toolsDir           = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64              = 'https://beta.unity3d.com/download/007fc09e806c/Windows64EditorInstaller/UnitySetup64-2017.1.1p1.exe'
-$checksum64         = '769b19cd75ebd13df91bc8f374514097e789aa4112f411bca178d188fda6eb4f'
+$url64              = 'https://beta.unity3d.com/download/b8e3f2d6c409/Windows64EditorInstaller/UnitySetup64-2017.1.1p2.exe'
+$checksum64         = 'e6d6aa7110f3c3469a90babb527205c3bdfa2d120290a58ff81de55ac067baf6'
 
 $args = '/S'
 
