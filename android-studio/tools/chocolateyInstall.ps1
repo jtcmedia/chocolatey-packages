68e7273d7cf786e5253772dd42c836c164035c80
jtcmedia <jason.cumiskey@gmail.com>
2017-10-27 10:53:12 -0700

    add android studio

---
+++ android-studio/tools/chocolateyInstall.ps1
@@ -0,0 +1,53 @@
+﻿
+# Common Functions and Config
+. (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Common.ps1')
+
+if (Test-Path ($installDir)) {
+    $uninstallExe = GetUninstallFile
+    
+    $params = @{
+        PackageName = $package;
+        FileType = "exe";
+        SilentArgs = "/S";
+        File = $uninstallExe;
+    }
+
+    Uninstall-ChocolateyPackage @params
+}
+
+
+$params = @{
+	PackageName = $package;
+	Url = "https://dl.google.com/dl/android/studio/install/$majorVersion/android-studio-ide-$buildVersion-windows.exe"
+
+	InstallerType = "exe";
+	SilentArgs = "/S";	
+	validExitCodes = @(0,1223);
+	Checksum = "$checksum"
+	ChecksumType = 'sha256'
+}
+
+Install-ChocolateyPackage @params
+	
+$customArgs = $env:chocolateyPackageParameters
+$settings = GetArguments $customArgs
+$studioExe = GetStudioExe
+
+if ($settings.addtodesktop -eq "true")
+{
+	Write-Host "AddToDesktop Value: " $settings.addtodesktop
+	
+    $desktopPath = [Environment]::GetFolderPath("Desktop")
+    $iconPath = Join-Path $(Split-Path $studioExe) 'studio.ico'
+    
+    Install-ChocolateyShortcut `
+        -ShortcutFilePath "$desktopPath\$package.lnk" `
+        -TargetPath "$studioExe" `
+        -IconLocation "$iconPath"
+}
+
+if ($settings.pinnedtotaskbar -eq "true")
+{
+	Write-Host "PinnedToTaskbar Value: " $settings.pinnedtotaskbar
+	Install-ChocolateyPinnedTaskBarItem $studioExe
+}
---
+++ android-studio/tools/chocolateyUninstall.ps1
@@ -0,0 +1,33 @@
+﻿# Common Functions and Config
+. (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Common.ps1')
+
+#$studioExe = (gci "${installdir}/bin/studio64.exe").FullName | sort -Descending | Select -first 1
+$studioExe = GetStudioExe
+
+# Remove Pinned Item if it exist
+Uninstall-ChocolateyPinnedTaskBarItem $studioExe
+
+# Remove Desktop Lnk if it exist
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+
+$desktopLink = Join-Path $desktop "$([System.IO.Path]::GetFileName($studioExe)).lnk"
+if (Test-Path ($desktopLink)) {Remove-Item $desktopLink -force}
+
+# Remove Android Studio Directory
+# Remove-Item -Recurse -Force $installDir
+$params = @{
+	PackageName = $package;
+	FileType = "exe";
+	SilentArgs = "/S";
+	File = GetUninstallFile;
+}
+
+
+Uninstall-ChocolateyPackage @params
+
+# if nothing else is in the $extractionPath besides Android Studio then delete it as well.
+if( (Get-ChildItem $extractionPath | Measure-Object).Count -eq 0)
+{
+	Write-Host "Deleting $extractionPath"
+	Remove-Item -Force $extractionPath
+}
