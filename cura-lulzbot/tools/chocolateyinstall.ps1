f102969dc07b90eabf55597545860e8c234bc286
jtcmedia <jason.cumiskey@gmail.com>
2018-02-24 02:42:16 -0800

    add cura-lulzbot pkg

---
+++ cura-lulzbot/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32      = 'http://download.lulzbot.com/Software/cura-lulzbot/windows/cura-lulzbot_2.6.69_win32.exe'
+$checksum32 = '98b7ed631c72f3175f9ba26f6c79e8678468b4c6730db336c6bab311ac6d5fa4'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url32
+  softwareName  = 'cura-lulzbot*'
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
