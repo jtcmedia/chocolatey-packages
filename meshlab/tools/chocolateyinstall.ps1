239f77af4b4ce5bef3b2d593f19e8d2f8234a3c8
jtcmedia <jason.cumiskey@gmail.com>
2018-04-05 00:32:00 -0700

    add meshlab

---
+++ meshlab/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Get-Item "$toolsDir\*x64.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file          = $fileLocation
+  softwareName  = 'MeshLab*'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
---
+++ meshlab/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://github.com/cnr-isti-vclab/meshlab/releases'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\VERIFICATION.txt" = @{
+            "(?i)(\s+x64:).*"           = "`${1} $($Latest.URL64)"
+            "(?i)(\s+checksum64:).*"    = "`${1} $($Latest.Checksum64)"
+            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+        }
+    }
+}
+
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = 'exe$'
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+        
+    $version = ($url -split '/' | select -Last 1 -Skip 1).Substring(1)
+    
+    $url64 = 'https://github.com' + $url
+    
+    return @{ URL64 = $url64; Version = $version }
+}
+
+update -ChecksumFor none
