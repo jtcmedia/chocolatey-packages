da53cdff460fdb3b9c6e97c893e742b8579960fa
jtcmedia <jason.cumiskey@gmail.com>
2018-04-17 01:19:20 -0700

    add handbrake.install

---
+++ handbrake.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Get-Item "$toolsDir\*_x64.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = $fileLocation
+  fileType      = 'EXE'
+  softwareName  = 'HandBrake*'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
