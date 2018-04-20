db53987b14c59e6e7acccf23e2badfaa99fd2948
Chocolatey <chocolatey@realdimensions.net>
2018-04-20 07:39:02 +0000

    AU: 1 updated - unity-android [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/5280b55e1c8125381a1fd227799a5c519244d64f

--- unity-android/tools/chocolateyinstall.ps1
+++ unity-android/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-android'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://netstorage.unity3d.com/unity/fc1d3344e6ea/TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-2017.3.1f1.exe'
-$checksum64     = 'd1132435863bbcdab5ea4d9280ca095d464d95802961de3b197d29e5eb0b49bc'
+$url64          = 'https://netstorage.unity3d.com/unity/9231f953d9d3/TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-2017.4.1f1.exe'
+$checksum64     = 'a176f9bbccdf06e405e3f4235617be428cfd1451d1a04175251973e0e7667401'
 
 $packageArgs = @{
   packageName    = $packageName
