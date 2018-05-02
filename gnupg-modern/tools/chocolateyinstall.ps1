8625af2389a708cc03631b8e8c713951e746bc60
Chocolatey <chocolatey@realdimensions.net>
2018-05-02 21:57:21 +0000

    AU: 1 updated - gnupg-modern [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/69582d895e114ce6988bd907743c8fda5982a4a6

--- gnupg-modern/tools/chocolateyinstall.ps1
+++ gnupg-modern/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'gnupg-modern'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.gnupg.org/ftp/gcrypt/binary/gnupg-w32-2.2.6_20180409.exe'
-$checksum32 = '5d426b19d9cc3d7d3c0db0dfb58af1b6a36f4ea1cfd55901bb6864e865fcccc6'
+$url        = 'https://www.gnupg.org/ftp/gcrypt/binary/gnupg-w32-2.2.7_20180502.exe'
+$checksum32 = '2d7ffedb1737e4dbed7f89377c3273b5cc27ee0fd9c70a417bd602203fc419ee'
 
 $packageArgs = @{
   packageName   = $packageName
