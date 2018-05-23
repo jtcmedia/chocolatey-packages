4104753a0e64449c0a47d7112d4c3e936ad91503
jtcmedia <jason.cumiskey@gmail.com>
2018-05-22 22:39:53 -0700

    updt->3.85

--- torguard-client/tools/chocolateyinstall.ps1
+++ torguard-client/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName= 'torguard-client'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://updates.torguard.biz/Software/Windows/torguard-setup-latest.exe'
-$checksum32 = 'ea3da1c544b341b4b9afdc0276c154106243e2090b3619a9afd3f765271c3a38'
+$checksum32 = '712c610f93bd9321eb05a1adcb16d4719a6a5d9d9198a123f9be30c0fb17e348'
 
 $packageArgs = @{
   packageName   = $packageName
