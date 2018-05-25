1fa872a4d1a344852868d5407c8e1649efa841bc
Chocolatey <chocolatey@realdimensions.net>
2018-05-25 03:11:33 +0000

    AU: 3 updated - ds4windows heroku-cli slic3r-prusa [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/89ff32917f56def36fcce8906f7d2b107967e4e5

--- ds4windows/tools/chocolateyinstall.ps1
+++ ds4windows/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName  = $env:ChocolateyPackageName
 $packageTitle = $env:ChocolateyPackageTitle
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64        = 'https://github.com/Ryochan7/DS4Windows/releases/download/v1.4.118/DS4Windows_1.4.118_x64.zip'
-$checksum64   = 'a76164fb4320ae22184efcd9d6b74b1ddf946f745ef79bb4061183a5caefa042'
+$url64        = 'https://github.com/Ryochan7/DS4Windows/releases/download/v1.4.119/DS4Windows_1.4.119_x64.zip'
+$checksum64   = 'c33d9821ffa1cecff8fa0e1cbe6c939f555838d7a5892dc818af6dd17d7c6687'
 
 $packageArgs = @{
   packageName   = $packageName
