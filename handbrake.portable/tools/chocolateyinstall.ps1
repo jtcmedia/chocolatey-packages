13a2dd96ebf52b60fba49c82731fca7cb373024e
jtcmedia <jason.cumiskey@gmail.com>
2018-06-18 21:47:13 -0700

    rm installer after finished

--- handbrake.portable/tools/chocolateyinstall.ps1
+++ handbrake.portable/tools/chocolateyinstall.ps1
@@ -3,3 +3,6 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $zipFile = Get-Item "$toolsDir\*_64.zip"
 
 Get-ChocolateyUnzip $zipFile $toolsDir
+
+#Don't need installer zip anymore
+rm $toolsDir\*.zip -ea 0 -force
