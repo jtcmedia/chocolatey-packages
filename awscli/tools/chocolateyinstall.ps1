bf012001a9fb15fe38b2e5c6b79896040b2e7365
Chocolatey <chocolatey@realdimensions.net>
2018-06-21 03:10:32 +0000

    AU: 2 updated - awscli heroku-cli [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/e3a93b7950e700858822c2ccda2413644c22a85b

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.15.41.msi'
-$checksum   = 'd30f9a7367e48c9f8cbc6e3d4df47e7fa9c0f5d354abf53c9a9a6e8477d4ca3d'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.15.41.msi'
-$checksum64 = 'f6ac044610dee3033bb5560f41e1053999874c146c75f677210fdd27994ef1aa'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32-1.15.42.msi'
+$checksum   = '6ffbb4a500de88daebabc752c445607291e5c959ab62fa5939814543ea8c4318'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64-1.15.42.msi'
+$checksum64 = 'e129654f18303a2d6145885064858fc533e763d9af7e4fd3cea0d82beb6911fd'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
