2e9f5f2f9ccfbc602a5bcc10b8755960b1eda5de
jtcmedia <jason.cumiskey@gmail.com>
2018-08-12 21:50:16 -0700

    AU: 2 updated - unity-android unity-win-il2cpp [skip ci]

--- unity-android/tools/chocolateyinstall.ps1
+++ unity-android/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-android'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://netstorage.unity3d.com/unity/c18cef34cbcd/TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-2018.2.2f1.exe'
-$checksum64     = 'd3e8844a9ca10648106fd7e3a1ca49f8a34509f9f435a733e39875ea011341a1'
+$url64          = 'https://netstorage.unity3d.com/unity/1431a7d2ced7/TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-2018.2.3f1.exe'
+$checksum64     = 'b01748d09b0744713ff56dd0d6f0211ee5dd8edbdb1f9dd1fa305023ac3d0c87'
 
 $packageArgs = @{
   packageName    = $packageName
