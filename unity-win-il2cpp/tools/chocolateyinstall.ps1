6c93a10826a89b9404d90a38dc696677c4f05137
jtcmedia <jason.cumiskey@gmail.com>
2018-08-19 21:18:46 -0700

    unity 2018.2 added windows non-uwp il2cpp support

---
+++ unity-win-il2cpp/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-il2cpp'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64          = 'https://netstorage.unity3d.com/unity/cb262d9ddeaf/TargetSupportInstaller/UnitySetup-Windows-IL2CPP-Support-for-Editor-2018.2.4f1.exe'
+$checksum64     = '4560c3a261919ef2b93ad811e8550563525f25673c1658951f2d0a57ae48a97a'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url64bit       = $url64
+  checksum       = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
