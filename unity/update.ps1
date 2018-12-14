17f2add508d073e6fd7613e2ff6311c3e99a3aa0
jtcmedia <jason.cumiskey@gmail.com>
2018-12-13 20:48:59 -0800

    unity whats-new page changed, fixes for major releases

--- unity/update.ps1
+++ unity/update.ps1
@@ -17,17 +17,18 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $major_releases -UseBasicParsing
     
     $regex = '.exe$'
-    $url = $download_page.links | ? href -match $regex | select -First 1 -Skip 2 -expand href
+    $editor_url = $download_page.links | ? href -match $regex | select -First 1 -Skip 1 -expand href
+    $android_url = $download_page.links | ? href -match $regex | select -First 1 -Skip 2 -expand href
     
-    if ($url -eq $null) {
+    if ($editor_url -eq $null) {
         # it's a minor release
         $download_page = Invoke-WebRequest -Uri $minor_releases -UseBasicParsing
         $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
     } 
     
-    $version = $url -split '-|f' | select -Last 1 -Skip 1
-    $release = $url -split 'f' | select -Last 1
-    $url_start = $url -split 'Windows64EditorInstaller' | select -First 1
+    $version = $android_url -split '-|f' | select -Last 1 -Skip 1
+    $release = $android_url -split 'f' | select -Last 1
+    $url_start = $editor_url -split 'Windows64EditorInstaller' | select -First 1
     
     $unity_data = @{}
     $unity_data["version"] = "$($version)"
@@ -36,7 +37,7 @@ function global:au_GetLatest {
     
     $unity_data | Export-CliXml $PSScriptRoot\..\_unity.xml
     
-    return @{ URL64 = $url -replace 'http:', 'https:'; Version = $version }
+    return @{ URL64 = $editor_url -replace 'http:', 'https:'; Version = $version }
 }
 
 update -ChecksumFor 64
