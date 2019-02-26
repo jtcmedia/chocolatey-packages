f07932551012b5d435e4d9d215e2fb8472187032
jtcmedia <jason.cumiskey@gmail.com>
2019-02-26 11:14:33 -0800

    d/l url changed

--- kicad/tools/chocolateyinstall.ps1
+++ kicad/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'http://downloads.kicad-pcb.org/windows/stable/kicad-5.0.2_1-i686.exe'
-$url64      = 'http://downloads.kicad-pcb.org/windows/stable/kicad-5.0.2_1-x86_64.exe'
+$url32      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.0.2_1-i686.exe'
+$url64      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.0.2_1-x86_64.exe'
 $checksum32 = 'fab44234d6fa91b43c460e2ede5e9cf292c11f0f3a0618f429452155678ff337'
 $checksum64 = '3931c930aa25a09d13e18742733c7b4874c814f5d811c010b056c5c3b9352c80'
 
