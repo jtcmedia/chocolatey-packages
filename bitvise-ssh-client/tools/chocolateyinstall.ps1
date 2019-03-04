a39ce3668095b573f10d52c7da88cc011108952b
Chocolatey <chocolatey@realdimensions.net>
2019-03-04 03:07:00 +0000

    AU: 2 updated - bitvise-ssh-client slic3r-prusa [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/3b28664efa43c2b4c44b07ccc9f537fafa9106d4

--- bitvise-ssh-client/tools/chocolateyinstall.ps1
+++ bitvise-ssh-client/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName= 'bitvise-ssh-client'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://dl.bitvise.com/BvSshClient-Inst.exe'
-$checksum32 = '6362984b572a2c7b8635fe7375b58c7957a89b153b7504a94db4efb5bd13c82a'
+$checksum32 = '95ce56d0e8112cc175bc8441b67898dde7353fe7d0a65e848a06d7f0dea9e989'
 
 $packageArgs = @{
   packageName   = $packageName
