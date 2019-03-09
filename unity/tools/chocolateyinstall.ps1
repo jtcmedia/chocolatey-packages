8c7f521ddf351a95c55e5390902798d40ed441e3
jtcmedia <jason.cumiskey@gmail.com>
2019-03-08 20:47:41 -0800

    updt -> 2018.3.8

--- unity/tools/chocolateyinstall.ps1
+++ unity/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName        = 'unity'
 $toolsDir           = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64              = 'https://download.unity3d.com/download_unity/9e14d22a41bb/Windows64EditorInstaller/UnitySetup64.exe'
-$checksum64         = 'd6447e215d70b071ee12aa4abc7675a3898ad136432c14646fa2a67cb26bc791'
+$url64              = 'https://download.unity3d.com/download_unity/fc0fe30d6d91/Windows64EditorInstaller/UnitySetup64.exe'
+$checksum64         = 'fdceb1a136b40d3423b6f90da07b4df4972e4f02974a9fc0ced9002b2b7f6704'
 
 $args = '/S'
 
