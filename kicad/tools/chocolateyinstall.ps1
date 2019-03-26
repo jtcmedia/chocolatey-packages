5b1bf40c3aea18a25fab2720a9b07890a7ce586e
jtcmedia <jason.cumiskey@gmail.com>
2019-03-26 11:03:05 -0700

    change to use github d/l mirror; got complaint about cern

--- kicad/tools/chocolateyinstall.ps1
+++ kicad/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.1.0_1-i686.exe'
-$url64      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.1.0_1-x86_64.exe'
+$url32      = 'https://github.com/KiCad/kicad-winbuilder/releases/download/5.1.0/kicad-5.1.0_1-i686.exe'
+$url64      = 'https://github.com/KiCad/kicad-winbuilder/releases/download/5.1.0/kicad-5.1.0_1-x86_64.exe'
 $checksum32 = 'b954c71e34fad4adad97563a341c05ffd1a0c47f3349854503c779c34df0f0b0'
 $checksum64 = '39b24f6e0492167ab688f45efd1bded07687a69b82e06c356ffa23cbd05e5d76'
 
