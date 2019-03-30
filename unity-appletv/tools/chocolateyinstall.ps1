496ed1ef153a3c4f13cad7a0ef300eca02f90280
jtcmedia <jason.cumiskey@gmail.com>
2019-03-29 21:09:55 -0700

    AU: 2 updated - unity-appletv unity-docs [skip ci]

--- unity-appletv/tools/chocolateyinstall.ps1
+++ unity-appletv/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-appletv'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/f88de2c96e63/TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-2018.3.10f1.exe'
-$checksum64     = '6017b79123b56567896dfc3b8aa9d059a17be13cc34e5f4f052d7d9e4dfaeee5'
+$url64          = 'https://download.unity3d.com/download_unity/5063218e4ab8/TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-2018.3.11f1.exe'
+$checksum64     = '93d5e90299e85ddc01c055bbfffa742c6c56556f21173c8cf1923b24d9c07621'
 
 $packageArgs = @{
   packageName    = $packageName
--- unity-docs/tools/chocolateyinstall.ps1
+++ unity-docs/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-docs'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/f88de2c96e63/WindowsDocumentationInstaller/UnityDocumentationSetup-2018.3.10f1.exe'
-$checksum64     = '55ace24c2d33041a61f12d36bb953f2f75f74b6a55f2ffe79f4c7cf73a58bbaa'
+$url64          = 'https://download.unity3d.com/download_unity/5063218e4ab8/WindowsDocumentationInstaller/UnityDocumentationSetup-2018.3.11f1.exe'
+$checksum64     = 'c905b4a580e31168573af3e236dd1d68d345faca3b958e740ad944c20261717c'
 
 $packageArgs = @{
   packageName    = $packageName
