f29b1abfc21ddf3679b7caffe69aee913c6c4354
Chocolatey <chocolatey@realdimensions.net>
2019-04-17 18:01:04 +0000

    AU: 1 updated - unity-lumin [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/17ebb2418120a8f7ac9150fb86776816f42ba8d4

--- unity-lumin/tools/chocolateyinstall.ps1
+++ unity-lumin/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-lumin'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/8afd630d1f5b/TargetSupportInstaller/UnitySetup-Linux-Support-for-Editor-2018.3.12f1.exe'
-$checksum64     = '710295d2f1eb6592a99baf02de2c41144dcffecd261db3afe1a973dfedc961eb'
+$url64          = 'https://download.unity3d.com/download_unity/292b93d75a2c/TargetSupportInstaller/UnitySetup-Lumin-Support-for-Editor-2019.1.0f2.exe'
+$checksum64     = 'db2a313b7b0e9d983a8e416459174dfa57b96955108b97b3b47bd03dcbb6fa53'
 
 $packageArgs = @{
   packageName    = $packageName
