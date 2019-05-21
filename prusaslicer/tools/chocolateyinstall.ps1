65a943732bc72b187f358edf899bb0e186c762f1
jtcmedia <jason.cumiskey@gmail.com>
2019-05-20 21:38:47 -0700

    add prusaslicer

---
+++ prusaslicer/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing 64 bit version"; Get-Item "$toolsDir\*_x64.zip"
+} else { Write-Host "Installing 32 bit version"; Get-Item "$toolsDir\*_x32.zip" }
+
+Get-ChocolateyUnzip $installerFile $toolsDir
+
+$files = Get-ChildItem $toolsDir -Include *.exe -Recurse
+
+foreach ($file in $files) {
+  if (!($file.Name.Equals("prusa-slicer.exe"))) {
+    #generate an ignore file
+    New-Item "$file.ignore" -type file -Force | Out-Null
+  }
+  else {
+    New-Item "$file.gui" -type file -Force | Out-Null
+  }
+}
+
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath "$desktopPath\PrusaSlicer.lnk" `
+  -TargetPath "$env:ChocolateyInstall\bin\prusa-slicer.exe"
+
+#Don't need installer zips anymore
+rm $toolsDir\*.zip -ea 0 -force
