27a4dbf3a763c408347c40e111f0bfb4147fac3d
jtcmedia <jason.cumiskey@gmail.com>
2019-06-24 22:48:25 -0700

    only 64 bit binary released now

--- fs-uae/update.ps1
+++ fs-uae/update.ps1
@@ -5,9 +5,7 @@ $releases = 'https://fs-uae.net/download#windows'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
@@ -17,18 +15,18 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     
     $regex = '.exe$'
-    $url = $download_page.links | ? href -match $regex | select -First 2 -expand href
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
     
-    $version = $url[0] -split '_' | select -Index 1
+    #$version = $url[0] -split '_' | select -Index 1
+    $version = $url -split '_' | select -First 1 -Skip 1
     # the author sometimes amends, for example, 2.8.1u3
     # for stable releases. Change to 2.8.1.3
-    $version = $version -replace '[u]', '.'
+    #$version = $version -replace '[u]', '.'
     
-    $url32 = 'https://fs-uae.net' + $url[0]
-    $url64 = 'https://fs-uae.net' + $url[1]
+    $url64 = 'https://fs-uae.net' + $url
     
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    $Latest = @{ URL64 = $url64; Version = $version }
     return $Latest
 }
 
-update
+update -ChecksumFor 64
