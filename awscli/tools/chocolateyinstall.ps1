a82bbd151041f0de93cc1d6381d571c2a7c429d6
Chocolatey <chocolatey@realdimensions.net>
2019-07-03 03:06:29 +0000

    AU: 1 updated - awscli [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/36bac5c79c5159f381fa2e2ac9cbb60ece651242

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.16.191.msi'
-$checksum   = '4e63275893fbbf23785170938b35c03b3768f4c58f2523b496bd976c63518da5'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.16.191.msi'
-$checksum64 = '37c50221f33a60dc6b5b437a83324e6025252ca080b1e24caa0eae3621048fbd'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.16.192.msi'
+$checksum   = 'c4ae74c8032e6a4decc8ed711d7e8bd88c42828399d83ae3fcb6a15d89dba1a0'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.16.192.msi'
+$checksum64 = 'dbf9b6aec7ea3397a8c234b2b567b55d2a5c4ef2e1d486ab628de37bace72cc7'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
