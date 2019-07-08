3fcd7154c53f84f5074eba532de10d4f795324c0
jtcmedia <jason.cumiskey@gmail.com>
2019-07-08 10:23:36 -0700

    make embedded

--- faas-cli/tools/chocolateyinstall.ps1
+++ faas-cli/tools/chocolateyinstall.ps1
@@ -2,15 +2,5 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'faas-cli.exe'
-$url        = 'https://github.com/openfaas/faas-cli/releases/download/0.8.19/faas-cli.exe'
-$checksum   = 'fa8aec5e83764c18402a72fe99c6e82f2941bb0ff988845e00b6b622f5371b0e'
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  url           = $url
-  fileFullPath  = $fileLocation
-  checksum      = $checksum
-  checksumType  = 'sha256'
-}
-
-Get-ChocolateyWebFile @packageArgs
+Install-BinFile -Name $env:ChocolateyPackageName -Path $fileLocation
--- faas-cli/update.ps1
+++ faas-cli/update.ps1
@@ -2,28 +2,34 @@ import-module au
 
 $releases = 'https://github.com/openfaas/faas-cli/releases'
 
-
 function global:au_SearchReplace {
     @{
-        ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"                   = "`${1} $($Latest.URL32)"
+          "(?i)(Get-RemoteChecksum).*"        = "`${1} $($Latest.URL32)"
+          "(?i)(\s+checksum_from_file:).*"    = "`${1} $($Latest.ChecksumUrl)"
+          "(?i)(\s+checksum32:).*"            = "`${1} $($Latest.Checksum32)"
         }
     }
 }
 
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
     $regex = '.exe$'
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+
+    $regex = '.exe.sha256$'
+    $checksum_url = $download_page.links | ? href -match $regex | select -First 1 -expand href
     
     $version = $url -split '/' | select -Last 1 -Skip 1
     
     $url32 = 'https://github.com' + $url
+    $checksum_url = 'https://github.com' + $checksum_url
     
-    return @{ URL32 = $url32; Version = $version }
+    return @{ URL32 = $url32; Version = $version; ChecksumUrl = $checksum_url }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
