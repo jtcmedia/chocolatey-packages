12d67a60dcf6c5ae4a17bb3ac253e846efe3bde9
jtcmedia <jason.cumiskey@gmail.com>
2019-07-08 21:23:41 -0700

    add uninstall

---
+++ faas-cli/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'faas-cli.exe'
+
+Uninstall-BinFile -Name $env:ChocolateyPackageName -Path $fileLocation
