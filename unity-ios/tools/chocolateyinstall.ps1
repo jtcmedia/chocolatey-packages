d390a8a93616bfb5228fbde3d2e053d920234dba
Chocolatey <chocolatey@realdimensions.net>
2019-07-15 03:07:13 +0000

    AU: 2 updated - unity-il2cpp unity-ios [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/7df06da829fb6839e45103e6e7f6826a9ff9842f

--- unity-ios/tools/chocolateyinstall.ps1
+++ unity-ios/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-ios'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/d5f1b37da199/TargetSupportInstaller/UnitySetup-iOS-Support-for-Editor-2019.1.9f1.exe'
-$checksum64     = '1d41272646eedfc4f77527f1ae23ed5bcc35dff45db4f1c57d7816c88da1b72b'
+$url64          = 'https://download.unity3d.com/download_unity/f007ed779b7a/TargetSupportInstaller/UnitySetup-iOS-Support-for-Editor-2019.1.10f1.exe'
+$checksum64     = 'f03e17101ebfffeb2a293e37a9be3c8260984fe18680639b0a3692bdfff01a72'
 
 $packageArgs = @{
   packageName    = $packageName
