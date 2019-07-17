248c4b0f69e5bed20bccabd15c432515b13a630c
Chocolatey <chocolatey@realdimensions.net>
2019-07-17 03:06:18 +0000

    AU: 1 updated - unity-facebook [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/5666ab83769f3f7b8bdb44ae1d2eaa1017f4749b

--- unity-facebook/tools/chocolateyinstall.ps1
+++ unity-facebook/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-facebook'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/d5f1b37da199/TargetSupportInstaller/UnitySetup-Facebook-Games-Support-for-Editor-2019.1.9f1.exe'
-$checksum64     = '176ae2a079b143baab87066e2732447b766783ab87eef1a1ec66a193f250971d'
+$url64          = 'https://download.unity3d.com/download_unity/f007ed779b7a/TargetSupportInstaller/UnitySetup-Facebook-Games-Support-for-Editor-2019.1.10f1.exe'
+$checksum64     = '0499b60000b9fb136573b4dc6c2ec42cdd8b9fac1cd09134dd3ad3db5f13a9b8'
 
 $packageArgs = @{
   packageName    = $packageName
