cb989e6ad2da7d641d65167cb4a5dbcd9841e79d
Chocolatey <chocolatey@realdimensions.net>
2019-07-29 03:08:58 +0000

    AU: 1 updated - unity-docs [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/944986593981fc5418dda3cef4cf3b7b56ca2403

--- unity-docs/tools/chocolateyinstall.ps1
+++ unity-docs/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-docs'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/9b001d489a54/WindowsDocumentationInstaller/UnityDocumentationSetup-2019.1.11f1.exe'
-$checksum64     = '31035a3ce042698e409cebc8d436802528830f75ea2cd9e2ba838fafde85432c'
+$url64          = 'https://download.unity3d.com/download_unity/f04f5427219e/WindowsDocumentationInstaller/UnityDocumentationSetup-2019.1.12f1.exe'
+$checksum64     = '9ae55c503e848201a9a959017d61afd210b48934e015e61ec04b030edb5bae6c'
 
 $packageArgs = @{
   packageName    = $packageName
