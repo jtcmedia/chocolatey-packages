49d283e3898c6bd5ffc5966cf27c18566a0a16e0
jtcmedia <jason.cumiskey@gmail.com>
2019-07-31 21:30:10 -0700

    remove un-needed install-binfile as exe in tools dir automatically shimmed

--- faas-cli/tools/chocolateyinstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'faas-cli.exe'
-
-Install-BinFile -Name $env:ChocolateyPackageName -Path $fileLocation
