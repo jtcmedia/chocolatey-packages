9c2cc5cfd123c475558b62a45b3afe63b1f44c54
Chocolatey <chocolatey@realdimensions.net>
2019-08-01 03:10:44 +0000

    AU: 3 updated - cura-lulzbot faas-cli nodejs-lts [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/ba075c7df338520d59bd54f229cbaf0e7c9c9e03

--- cura-lulzbot/tools/chocolateyinstall.ps1
+++ cura-lulzbot/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'http://download.lulzbot.com/Software/cura-lulzbot/windows/cura-lulzbot_3.6.15_win32.exe'
-$checksum32 = '665983db1603c12310f3ad5f02401b33e2f8856e53936b5e9bcdb8e663878499'
+$url32      = 'http://download.lulzbot.com/Software/cura-lulzbot/windows/cura-lulzbot_3.6.18_win32.exe'
+$checksum32 = '0bdf66512e5dbc17a72f2ea344ed5656af0dd13788f576219424ff95c6d7a177'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
