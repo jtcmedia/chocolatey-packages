099e9b2435d42356eca2f8ff5a22a6b1a641f80a
jtcmedia <jason.cumiskey@gmail.com>
2019-09-02 00:44:37 -0700

    use file64, don't need to check if 32 bit OS, choco will take care of it

--- balena-cli/tools/chocolateyinstall.ps1
+++ balena-cli/tools/chocolateyinstall.ps1
@@ -1,14 +1,10 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing..."; Get-Item "$toolsDir\*_x64.exe"
-} else { Write-Error "balena-cli can only be installed on a 64 bit OS" }
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  file          = $installerFile
+  file64        = Get-Item "$toolsDir\*_x64.exe"
   softwareName  = 'balena-cli'
   silentArgs   = '/S'
   validExitCodes= @(0)
