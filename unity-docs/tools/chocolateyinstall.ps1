e844651655de8644f148605344c87292c77329c9
jtcmedia <jason.cumiskey@gmail.com>
2019-09-01 21:38:33 -0700

    updt -> 2019.2.3

--- unity-docs/tools/chocolateyinstall.ps1
+++ unity-docs/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-docs'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/ab112815d860/WindowsDocumentationInstaller/UnityDocumentationSetup-2019.2.2f1.exe'
-$checksum64     = '66eb4608fd3eedd2a78443c82c70dc251b3dbfd7a1d61f75ced0d90f3e05dc4f'
+$url64          = 'https://download.unity3d.com/download_unity/8e55c27a4621/WindowsDocumentationInstaller/UnityDocumentationSetup-2019.2.3f1.exe'
+$checksum64     = ''
 
 $packageArgs = @{
   packageName    = $packageName
