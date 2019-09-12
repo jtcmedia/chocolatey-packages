82599072e0691280ef71aee9ab20930b6c3cf635
Chocolatey <chocolatey@realdimensions.net>
2019-09-12 03:37:38 +0000

    AU: 1 updated - unity-appletv [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/0c7fdfdf78e06ac2bd38818334d187b8aa9ab92b

--- unity-appletv/tools/chocolateyinstall.ps1
+++ unity-appletv/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-appletv'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/c63b2af89a85/TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-2019.2.4f1.exe'
-$checksum64     = 'd0191fe4335ac46d9baf2b6aed57483e66947967eac8a1f9a5d75860a77b227d'
+$url64          = 'https://beta.unity3d.com/download/9dace1eed4cc/TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-2019.2.5f1.exe'
+$checksum64     = 'ae7f8bb59c68dcf3f176b446be14b0caa86ee35b4d06e81a64d6eb0d09138e69'
 
 $packageArgs = @{
   packageName    = $packageName
