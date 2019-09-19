fad7babe1f2f1fdce06418574e8083faa2df4bfb
Chocolatey <chocolatey@realdimensions.net>
2019-09-19 04:57:56 +0000

    AU: 2 updated - unity-docs unity-facebook [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/82ec6982f804c59c4bbac226a50582d222849a9a

--- unity-docs/tools/chocolateyinstall.ps1
+++ unity-docs/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-docs'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://beta.unity3d.com/download/9dace1eed4cc/WindowsDocumentationInstaller/UnityDocumentationSetup-2019.2.5f1.exe'
-$checksum64     = 'ebe578f378f5a1025ee992aec52177d525e8542a371c6c501a0cac956bd4e329'
+$url64          = 'https://download.unity3d.com/download_unity/fe82a0e88406/WindowsDocumentationInstaller/UnityDocumentationSetup-2019.2.6f1.exe'
+$checksum64     = 'a1f650d71b4446f0590b00380466b7d4f2c3acb3659c8a22701e8222a2d00b00'
 
 $packageArgs = @{
   packageName    = $packageName
--- unity-facebook/tools/chocolateyinstall.ps1
+++ unity-facebook/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-facebook'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://beta.unity3d.com/download/9dace1eed4cc/TargetSupportInstaller/UnitySetup-Facebook-Games-Support-for-Editor-2019.2.5f1.exe'
-$checksum64     = 'bbab470f956af6b6dc08a64a4b832535df85ca30ce4e80cbfb82d3103727f00d'
+$url64          = 'https://download.unity3d.com/download_unity/fe82a0e88406/TargetSupportInstaller/UnitySetup-Facebook-Games-Support-for-Editor-2019.2.6f1.exe'
+$checksum64     = '184d3f910bd785e45092c2a09af00d6e07ba1f74c433319f54f124d882d33b12'
 
 $packageArgs = @{
   packageName    = $packageName
