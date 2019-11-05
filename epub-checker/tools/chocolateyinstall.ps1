6ab285fe2808f410bfa47e156181a0d78e7f334f
jtcmedia <jason.cumiskey@gmail.com>
2019-11-05 15:57:19 -0800

    add epub-checker

---
+++ epub-checker/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Get-Item "$toolsDir\*.zip"
+
+Get-ChocolateyUnzip -FileFullPath $fileLocation -Destination $toolsDir -PackageName $env:ChocolateyPackageName
+
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath "$desktopPath\pagina EPUB-Checker.lnk" `
+  -TargetPath "$env:ChocolateyInstall\bin\EPUB-Checker.exe"
+
+#Don't need installer zips anymore
+rm $toolsDir\*.zip -ea 0 -force
---
+++ epub-checker/tools/chocolateyuninstall.ps1
@@ -0,0 +1,2 @@
+﻿$desktopPath = [Environment]::GetFolderPath("Desktop")
+Remove-Item "$desktopPath\pagina EPUB-Checker.lnk" -ErrorAction SilentlyContinue -Force
