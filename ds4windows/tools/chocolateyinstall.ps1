c11505cbf1e6aa7d03f8af0056b8de4c92088030
Chocolatey <chocolatey@realdimensions.net>
2020-01-01 04:54:35 +0000

    AU: 1 updated - ds4windows [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/a6433a7b67b8e0431bca6ab70f56782234233b2d

--- ds4windows/tools/chocolateyinstall.ps1
+++ ds4windows/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageName  = $env:ChocolateyPackageName
 $packageTitle = $env:ChocolateyPackageTitle
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.0.1/DS4Windows_2.0.1_x86.zip'
-$checksum     = '0fb893a4ce9e8c361599ba28d0722ca979fa3906688edadd861cadb57877ef08'
-$url64        = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.0.1/DS4Windows_2.0.1_x64.zip'
-$checksum64   = 'ecd7915212b6e1d71eec23a0e93772d13125c6981068d64c1cbfbded96d23666'
+$url          = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.0.3/DS4Windows_2.0.3_x86.zip'
+$checksum     = '0743112ee9ea25e2dd5c17f99152f93dfe82c928af3367d5c3269c8529629be4'
+$url64        = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.0.3/DS4Windows_2.0.3_x64.zip'
+$checksum64   = '0f7e32cd680a9d7be17a64addb544a84cf24983a7f7c42956f66e670b92db703'
 
 $packageArgs = @{
   packageName   = $packageName
