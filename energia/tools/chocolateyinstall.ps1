dcb436627bd33bf7370aafcc53a90c7a0209a4ff
jtcmedia <jason.cumiskey@gmail.com>
2020-01-10 21:35:59 -0800

    rm embedded zip, too large for choco; updt to 1.8.10.23

--- energia/tools/chocolateyinstall.ps1
+++ energia/tools/chocolateyinstall.ps1
@@ -1,9 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$installerFile = Get-Item "$toolsDir\*_x32.zip"
+$url32          = 'http://energia.nu/downloads/downloadv4.php?file=energia-1.8.10E23-windows.zip'
+$checksum32     = '878d0082a1dbbd42dccaea1a5f2d4ece9c0a5d47a6bc6778c4392837e2f228eb'
 
-Get-ChocolateyUnzip $installerFile $toolsDir
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url32
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
 
 $files = Get-ChildItem $toolsDir -Include *.exe -Recurse
 
@@ -22,6 +31,3 @@ $desktopPath = [Environment]::GetFolderPath("Desktop")
 Install-ChocolateyShortcut `
   -ShortcutFilePath "$desktopPath\Energia.lnk" `
   -TargetPath "$env:ChocolateyInstall\bin\energia.exe"
-
-#Don't need installer zips anymore
-rm $toolsDir\*.zip -ea 0 -force
--- energia/update.ps1
+++ energia/update.ps1
@@ -4,31 +4,25 @@ $releases = 'https://energia.nu/download/'
 
 function global:au_SearchReplace {
     @{
-        ".\tools\VERIFICATION.txt" = @{
-            "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
-            "(?i)(\s+checksum32:).*"     = "`${1} $($Latest.Checksum32)"
-            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
-        }
+      ".\tools\chocolateyinstall.ps1" = @{
+          "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+          "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      }
     }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase $Latest.FileName }
 
 function global:au_GetLatest {
-    $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-    
+  
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
     $regex = 'windows.zip$'
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
     
-    $file = $url -split '=' | select -Last 1
-    
     $version = $url -split '-' | select -Index 1
     $version = $version -replace 'E', '.'
     
-    return @{ URL32 = $url; Version = $version; FileName = $file }
+    return @{ URL = $url; Version = $version }
 }
 
-update -ChecksumFor None
+update -ChecksumFor 32
