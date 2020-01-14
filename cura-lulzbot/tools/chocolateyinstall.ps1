da612b66da61c2ca5ee259176bc4ffa55c244fc0
jtcmedia <jason.cumiskey@gmail.com>
2020-01-14 14:11:16 -0800

    fix empty url/checksum fields

--- cura-lulzbot/tools/chocolateyinstall.ps1
+++ cura-lulzbot/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = ''
-$checksum32 = ''
+$url32      = 'https://gitlab.com/lulzbot3d/cura-le/cura-lulzbot/uploads/baeabfa19e27f4340db2c002cd0240e8/cura-lulzbot_3.6.20_win32.exe'
+$checksum32 = '1dc773e824dfd1891fa614dfafa700e0ce29ca3e91bc23353b0119890770fbaa'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
