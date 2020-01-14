ffd0e353c3751665fa1cf045fe8aa92088c3c082
jtcmedia <jason.cumiskey@gmail.com>
2020-01-14 12:02:42 -0800

    fix d/l url; closes #38

--- cura-lulzbot/tools/chocolateyinstall.ps1
+++ cura-lulzbot/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://lulzbot-download.s3-us-west-1.amazonaws.com/lulzbot/Software/cura-lulzbot/windows/cura-lulzbot_3.6.21_win32.exe'
-$checksum32 = '89e48830fdd3c84ae8c195ed0fc8abadfc0d2595c1f05b94900f0dba31b749b3'
+$url32      = ''
+$checksum32 = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- cura-lulzbot/update.ps1
+++ cura-lulzbot/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
     $regex = '.exe$'
-    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+    $url = $download_page.links | ? href -match $regex | select -First 1 -Skip 1 -expand href
 	
     $version = $url -split '_' | select -Last 1 -Skip 1
 	
