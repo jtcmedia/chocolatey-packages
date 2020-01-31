f704d2ee15899debfc918f4a71bcbe585ecac521
jtcmedia <jason.cumiskey@gmail.com>
2020-01-30 20:02:22 -0800

    unity linux added 'mono' to installer

--- unity-linux/update.ps1
+++ unity-linux/update.ps1
@@ -18,7 +18,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   
-    $url = $unity_data["url"] + "TargetSupportInstaller/UnitySetup-Linux-Support-for-Editor-" + $unity_data["version"] + "f" + $unity_data["release"]
+    $url = $unity_data["url"] + "TargetSupportInstaller/UnitySetup-Linux-Mono-Support-for-Editor-" + $unity_data["version"] + "f" + $unity_data["release"]
     
     
     return @{ URL64 = $url -replace 'http:', 'https:'; Version = $unity_data["version"] }
