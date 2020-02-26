e474987a5fb1e1547dfaef9e12eb0d9bfc108908
jtcmedia <jason.cumiskey@gmail.com>
2020-02-25 20:23:09 -0800

    add awscli-v2 pkg

---
+++ awscli-v2/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://awscli.amazonaws.com/AWSCLIV2.msi'
+$checksum64 = '64e056625156f024832917bc308e74a56079d47437c0ff6fb078dfc968cf384c'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url64bit      = $url64
+  softwareName  = 'AWS Command Line Interface v2*'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ awscli-v2/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://github.com/aws/aws-cli/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = 'zip$'
+    $zip_url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+        
+    $version = $zip_url -split '/|\.zip' | select -Last 1 -Skip 1
+    
+    if ($version -match '^2') {
+      $url64 = "https://awscli.amazonaws.com/AWSCLIV2.msi"
+    } else {
+      return 'ignore'
+    }
+    
+    return @{ URL64 = $url64; Version = $version; PackageName = 'awscli' }
+}
+
+update -ChecksumFor 64
