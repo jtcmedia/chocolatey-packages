d4889adb6134c1af87bbc56e9c1fda2148e61d44
jtcmedia <jason.cumiskey@gmail.com>
2020-03-26 15:34:05 -0700

    updt -> 2019.3.7

--- unity-ios/tools/chocolateyinstall.ps1
+++ unity-ios/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName    = 'unity-ios'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64          = 'https://netstorage.unity3d.com/unity/6437fd74d35d/TargetSupportInstaller/UnitySetup-iOS-Support-for-Editor-2019.3.7f1.exe'
-$checksum64     = ''
+$checksum64     = '4c5aaa985cf924383453ffd60f2c8073fd0ce85e27cd8805f6d854093b61752d'
 
 $packageArgs = @{
   packageName    = $packageName
