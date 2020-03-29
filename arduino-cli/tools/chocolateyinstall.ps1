9cad17663b7e8eb1c8508fd106fed7aae4442dbb
jtcmedia <jason.cumiskey@gmail.com>
2020-03-29 11:09:14 -0700

    add arduino-cli package

---
+++ arduino-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+
+$packageArgs = @{
+  PackageName     = $env:ChocolateyPackageName
+  FileFullPath    = gi $toolsPath\*_32bit.zip
+  FileFullPath64  = gi $toolsPath\*_64bit.zip
+  Destination     = $toolsPath
+}
+
+ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+rm $toolsPath\*.zip -ea 0
---
+++ arduino-cli/update.ps1
@@ -0,0 +1,42 @@
+import-module au
+
+$releases = 'https://github.com/arduino/arduino-cli/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"                   = "`${1} $($Latest.URL32)"
+          "(?i)(\s+x64:).*"                   = "`${1} $($Latest.URL64)"
+          "(?i)(\s+SHASUMS:).*"               = "`${1} $($Latest.SHASUMS)"
+          "(?i)(Get-RemoteChecksum).*"        = "`${1} $($Latest.URL64)"
+          "(?i)(\s+checksum32:).*"            = "`${1} $($Latest.Checksum32)"
+          "(?i)(\s+checksum64:).*"            = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = '.(txt|zip)$'
+    $url = $download_page.links | ? href -match $regex | select -First 3 -expand href | % { 'https://github.com' + $_ }
+
+    $version = $url[2] -split '_' | select -First 1 -Skip 1
+    
+    @{
+        URL32 = $url[1]
+        URL64 = $url[2]
+        Version = $version.Replace('v','')
+        SHASUMS = $url[0]
+        ReleaseNotes = "https://github.com/arduino/arduino-cli/releases/tag/${version}"
+    }
+}
+
+update -ChecksumFor none
