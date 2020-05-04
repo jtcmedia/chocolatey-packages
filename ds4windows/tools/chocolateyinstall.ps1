a41e75667439f94097ea4d0f976f1a0ef5570b02
Chocolatey <chocolatey@realdimensions.net>
2020-05-04 03:06:48 +0000

    AU: 3 updated - ds4windows handbrake.install handbrake.portable [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/a4cc6bc3c5030a9ead60a220958e62676ff138fe

--- ds4windows/tools/chocolateyinstall.ps1
+++ ds4windows/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageName  = $env:ChocolateyPackageName
 $packageTitle = $env:ChocolateyPackageTitle
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.0.16/DS4Windows_2.0.16_x86.zip'
-$checksum     = 'de71c07fab17dbaaf5bae0d4d16aef4e4ddb2034ae34a772d3e8a6740c78ccb2'
-$url64        = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.0.16/DS4Windows_2.0.16_x64.zip'
-$checksum64   = 'aec70ede22ca8fabbe340bbebd4c6595179a7dba8df200807650e62222732b3a'
+$url          = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.1.0/DS4Windows_2.1.0_x86.zip'
+$checksum     = '4f6b142bc7ca10d43ec49b3907a59a7fe13d576e4073d48248935825994ce2a2'
+$url64        = 'https://github.com/Ryochan7/DS4Windows/releases/download/v2.1.0/DS4Windows_2.1.0_x64.zip'
+$checksum64   = '711a7c629aca43be908f34604c323101070ba18a0bb3f12ddca2a46b1fcc41b8'
 
 $packageArgs = @{
   packageName   = $packageName
