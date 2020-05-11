59b09c63cfca2435f158416c3cbf9184a0b022f4
Chocolatey <chocolatey@realdimensions.net>
2020-05-11 03:06:28 +0000

    AU: 1 updated - bitvise-ssh-client [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/8881e50f82e76487a0fbd543fceba9ec7f86e4db

--- bitvise-ssh-client/tools/chocolateyinstall.ps1
+++ bitvise-ssh-client/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName= 'bitvise-ssh-client'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://dl.bitvise.com/BvSshClient-Inst.exe'
-$checksum32 = '8364527e936317dcc324f935a964cf8a04f022f1fdb639c99bd94b3fb4deb2a6'
+$checksum32 = '47a2b55d2b700ce4089226fce616aad2d5974c087b8e8adcea12e8b04017032d'
 
 $packageArgs = @{
   packageName   = $packageName
