cda5eab523b30db9a59d1ee9ac9790f2acd0f4c7
Chocolatey <chocolatey@realdimensions.net>
2020-06-17 03:06:41 +0000

    AU: 1 updated - awscli [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/7bb07b03ec0b02353437e30c04560c72a5806039

--- awscli-v2/tools/chocolateyinstall.ps1
+++ awscli-v2/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.0.22.msi'
-$checksum64 = 'f9970164e97e27db3cfbeb0115c1217d09e6b151af30fb7eaaee82b67160ad7c'
+$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.0.23.msi'
+$checksum64 = '21d5cccbe5fc0c694f1a7f90c1d278de8a075c60b70196f142a6462bc8de1532'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
