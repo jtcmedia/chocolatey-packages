313d21ee95ab794aaad8b924fc4fb26da920cd52
jtcmedia <jason.cumiskey@gmail.com>
2020-07-24 10:56:12 -0700

    add whatsnew as sep pkgs

---
+++ unity-android-whatsnew/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName    = 'unity-android'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64          = 'https://download.unity3d.com/download_unity/81610f64359c/TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-2019.4.5f1.exe'
+$checksum64     = '20e42e2048ab0e014914eaafdfc09fde44b5a6301f68e4d3ae52468c10292537'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ unity-whatsnew/update.ps1
@@ -0,0 +1,51 @@
+import-module au
+
+$releases = 'https://unity3d.com/unity/whats-new'
+
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(^[$]url64\s*=\s*)('.*')"                = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')"           = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $major_releases -UseBasicParsing
+    
+    $regex = 'UnitySetup64'
+    $editor_url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+
+    $regex = 'Android-Support-for-Editor'
+    $android_url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+
+    $version = $android_url -split '-|f' | select -Last 1 -Skip 1
+    $release = $android_url -split 'f' | select -Last 1
+    
+        
+    $url_start = $editor_url -split 'Windows64EditorInstaller' | select -First 1
+    
+    $unity_data = @{}
+    $unity_data["version"] = "$($version)"
+    $unity_data["url"] = "$($url_start)"
+    $unity_data["release"] = "$($release)"
+    
+    $unity_data | Export-CliXml $PSScriptRoot\..\_unity.xml
+
+    @{
+        URL64   = $editor_url
+        Version = $version
+        PackageName = 'unity'
+        URL_android  = $android_url
+        URL_appletv  = $url_start + "TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-" + $version + "f" + $release
+        URL_docs     = $url_start + "WindowsDocumentationInstaller/UnityDocumentationSetup.exe"
+        #URL_facebook = $url_start + "TargetSupportInstaller/UnitySetup-Facebook-Games-Support-for-Editor-" + $version + "f" + $release
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+  update -ChecksumFor 64
+  if ($global:au_old_force -is [bool]) { $global:au_force = $global:au_old_force }
+}
