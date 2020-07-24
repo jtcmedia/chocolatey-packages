960e5a88e9bf7f684557e816f77ac187a16bb1a3
jtcmedia <jason.cumiskey@gmail.com>
2020-07-24 10:55:34 -0700

    rm whatsnew page

--- unity/update.ps1
+++ unity/update.ps1
@@ -1,7 +1,6 @@
 import-module au
 
-$releases1 = 'https://unity3d.com/unity/whats-new'
-$releases2 = 'https://unity3d.com/get-unity/download/archive'
+$releases = 'https://unity3d.com/get-unity/download/archive'
 
 function global:au_SearchReplace {
     @{
@@ -13,20 +12,16 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases1 -UseBasicParsing
-    
+
     $regex = 'UnitySetup64'
+    
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
     $editor_url = $download_page.links | ? href -match $regex | select -First 1 -expand href
-    
-    if ($editor_url -eq $null) {
-        $download_page = Invoke-WebRequest -Uri $releases2 -UseBasicParsing
-        $editor_url = $download_page.links | ? href -match $regex | select -First 1 -expand href
-    }
-    
-    
+
     $version = $editor_url -split '-|f' | select -Last 1 -Skip 1
     $release = $editor_url -split 'f' | select -Last 1
-        
+
     $url_start = $editor_url -split 'Windows64EditorInstaller' | select -First 1
     
     $unity_data = @{}
@@ -37,11 +32,12 @@ function global:au_GetLatest {
     $unity_data | Export-CliXml $PSScriptRoot\..\_unity.xml
 
     @{
-        URL64   = $editor_url -replace 'http:', 'https:'
+        URL64   = $editor_url
         Version = $version
         URL_android  = $url_start + "TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-" + $version + "f" + $release
         URL_appletv  = $url_start + "TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-" + $version + "f" + $release
         URL_docs     = $url_start + "WindowsDocumentationInstaller/UnityDocumentationSetup.exe"
+        #URL_facebook = $url_start + "TargetSupportInstaller/UnitySetup-Facebook-Games-Support-for-Editor-" + $version + "f" + $release
     }
 }
 
