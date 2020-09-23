cc3885a6846ef4867d4522447ac068a0ea8d15c3
Jason C <cumiskey@camosun.bc.ca>
2020-09-23 09:32:31 -0700

    updt d/l url

--- torguard-client/update.ps1
+++ torguard-client/update.ps1
@@ -19,7 +19,7 @@ function global:au_GetLatest {
     
     $version = ($download_page.ParsedHtml.getElementsByTagName("td") | ? InnerHTML -match $regex | select -First 1).InnerText.Substring(1)
     
-    $url = "https://updates.torguard.biz/Software/Windows/torguard-setup-latest.exe"
+    $url = "https://torguard.net/downloads/torguard-setup-latest.exe"
     
     return @{ URL32 = $url; Version = $version }
 
