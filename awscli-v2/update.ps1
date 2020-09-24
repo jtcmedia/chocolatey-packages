1a78e4ec61e3185549c75ecdf473e9efe4ac1c62
Jason C <cumiskey@camosun.bc.ca>
2020-09-24 13:01:05 -0700

    releaseNotes point to specific release

--- awscli-v2/update.ps1
+++ awscli-v2/update.ps1
@@ -3,7 +3,11 @@ import-module au
 $releases = 'https://github.com/aws/aws-cli/releases'
 
 function global:au_SearchReplace {
-   @{
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ChangeLog)`$2"
+        }
+        
         ".\tools\chocolateyinstall.ps1" = @{
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
@@ -19,6 +23,8 @@ function global:au_GetLatest {
     $zip_url = $download_page.links | ? href -match $regex | select -First 1 -expand href
         
     $version = $zip_url -split '/|\.zip' | select -Last 1 -Skip 1
+
+    $changeLog = "https://github.com/aws/aws-cli/blob/v2/CHANGELOG.rst#" + $version.Replace('.','')
     
     if ($version -match '^2') {
       $url64 = "https://awscli.amazonaws.com/AWSCLIV2-$version.msi"
@@ -26,7 +32,12 @@ function global:au_GetLatest {
       return 'ignore'
     }
     
-    return @{ URL64 = $url64; Version = $version; PackageName = 'awscli' }
+    @{
+        URL64 = $url64
+        Version = $version
+        PackageName = 'awscli'
+        ChangeLog = $changeLog
+    }
 }
 
 update -ChecksumFor 64
