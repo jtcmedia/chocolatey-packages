2ecef887b359ad6fbfa327ae67510eee52ae5d85
Chocolatey <chocolatey@realdimensions.net>
2020-09-28 03:10:12 +0000

    AU: 2 updated - ds4windows kicad [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/17339595b2888598323bf5f1bfeba25a6f293b7d

--- kicad/tools/chocolateyinstall.ps1
+++ kicad/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.1.6_1-i686.exe'
-$url64      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.1.6_1-x86_64.exe'
-$checksum32 = 'd2f64d261a224d7bf348129d51605419d6226952d5d205df627ee8ca5bd4390d'
-$checksum64 = '831d99141ccbbd491f765c08a7eafb6a385c4bbd306026eca2b4402ed8325bd2'
+$url32      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.1.7_1-i686.exe'
+$url64      = 'https://kicad-downloads.s3.cern.ch/windows/stable/kicad-5.1.7_1-x86_64.exe'
+$checksum32 = '0e898c1da50edf6c01f9e303a84b604879a7976ca331b416a4674c0fdb326c41'
+$checksum64 = '6fa571c7ada9f5c7d5aef2c7c962ca4a51ac98ffb6077f8f9026d951f80281bb'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
