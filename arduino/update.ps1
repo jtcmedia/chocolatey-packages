1584319f786116cb7dfa487b7801a8c501c4abae
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-11-01 17:19:41 +0100

    Auto update

--- arduino/update.ps1
+++ arduino/update.ps1
@@ -1,38 +1,27 @@
 import-module au
 
-$releases = 'https://www.arduino.cc/en/software'
+$releases = 'https://github.com/arduino/Arduino/releases'    
 
 
 function global:au_SearchReplace {
     @{
-        ".\legal\VERIFICATION.txt" = @{
-            "(?i)(installer:).*"         = "`${1} $($Latest.URL32)"
-            "(?i)(\s+SHA512SUMS:).*"     = "`${1} $($Latest.SHA512SUMS)"
-            "(?i)(\s+sha256checksum:).*" = "`${1} $($Latest.Checksum32)"
-            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
-        }
+        'tools/chocolateyInstall.ps1' = @{
+			"(^[$]url\s*=\s*)('.*')"      			= "`$1'$($Latest.URL32)'"
+			"(^[$]checksum\s*=\s*)('.*')" 			= "`$1'$($Latest.Checksum32)'"
+			"(^[$]checksumType\s*=\s*)('.*')" 		= "`$1'$($Latest.ChecksumType32)'"
+		}
     }
 }
 
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
-
-
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $(((((Invoke-WebRequest -Uri $releases -UseBasicParsing).Links)) | Where-Object {$_ -match '.tar.xz'} | Select-Object -First 1).href)
     
-    $regex = '.exe$'
-    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
-    
-    $version = $url -split '-' | select -Last 1 -Skip 1
-    
-    $url_sha = $download_page.links | ? href -match '.txt$' | select -First 1 -Skip 1 -expand href
+    $version = $url -split '/' | select -Last 1 -Skip 1
     
     $url32 = 'https://downloads.arduino.cc/arduino-' + $version + '-windows.exe'
     
-    $sha512sums = 'https:' + $url_sha
-    
-    return @{ URL32 = $url32; SHA512SUMS = $sha512sums; Version = $version }
+    return @{ URL32 = $url32; Version = $version }
 }
 
-update -ChecksumFor none
+update -ChecksumFor 32
