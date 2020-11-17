5db9fd62f7bf2f4acb6e783bdd51084e1b871743
jtcmedia <jason.cumiskey@gmail.com>
2020-11-17 09:58:20 -0800

    d/l from github; use GUI version ZIP

--- handbrake.portable/update.ps1
+++ handbrake.portable/update.ps1
@@ -1,10 +1,14 @@
 import-module au
 
-$releases = 'https://handbrake.fr/downloads2.php'
+$releases = 'https://github.com/HandBrake/HandBrake/releases/latest'
 
 function global:au_SearchReplace {
     @{
-        ".\tools\VERIFICATION.txt" = @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
+        ".\legal\VERIFICATION.txt" = @{
             "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
             "(?i)(\s+checksum64:).*"     = "`${1} $($Latest.Checksum64)"
             "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
@@ -15,23 +19,18 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest {
-    $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-    
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
-    $regex = '.zip$'
-    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+    $regex = 'GUI.zip$'
+    $url64 = $download_page.links | ? href -match $regex | select -First 1 -expand href | % { 'https://github.com' + $_ }
 	
-    $version = $url -split '-' | select -Last 1 -Skip 2
+    $version = $url64 -split '-' | select -Last 1 -Skip 2
     
-    $url_get_request = 'https://handbrake.fr/' + $url
-    
-    $downloading_page = Invoke-WebRequest $url_get_request
-    
-    $url64 = $downloading_page.links | ? href -match $regex | select -First 1 -expand href
-    	
-    return @{ URL64 = $url64; Version = $version }
+    @{
+        URL64 = $url64
+        Version = $version
+        ReleaseNotes = "https://github.com/HandBrake/HandBrake/releases/tag/${version}"
+    }
 }
 
 update -ChecksumFor none
