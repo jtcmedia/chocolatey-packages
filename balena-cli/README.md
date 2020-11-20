36e1127cbb0e263c60c37cfaa1c46ab7269e44c6
jtcmedia <jason.cumiskey@gmail.com>
2020-11-20 10:46:50 -0800

    add README, add icon, Release Notes url

---
+++ balena-cli/README.md
@@ -0,0 +1,15 @@
+
+
+# balena CLI
+
+The official balena Command Line Interface.
+
+## About
+
+The balena CLI is a Command Line Interface for [balenaCloud](https://www.balena.io/cloud/) or
+[openBalena](https://www.balena.io/open/). It is a software tool available for Windows, macOS and
+Linux, used through a command prompt / terminal window. It can be used interactively or invoked in
+scripts. The balena CLI builds on the [balena API](https://www.balena.io/docs/reference/api/overview/)
+and the [balena SDK](https://www.balena.io/docs/reference/sdk/node-sdk/), and can also be directly
+imported in Node.js applications. The balena CLI is an [open-source project on
+GitHub](https://github.com/balena-io/balena-cli/), and your contribution is also welcome!
--- balena-cli/update.ps1
+++ balena-cli/update.ps1
@@ -1,9 +1,13 @@
 import-module au
 
-$releases = 'https://github.com/balena-io/balena-cli/releases'
+$releases = 'https://github.com/balena-io/balena-cli/releases/latest'
 
 function global:au_SearchReplace {
     @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+            
         ".\legal\VERIFICATION.txt" = @{
           "(?i)(\s+x64:).*"                   = "`${1} $($Latest.URL64)"
           "(?i)(Get-RemoteChecksum).*"        = "`${1} $($Latest.URL64)"
@@ -18,13 +22,15 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
     $regex = '.exe$'
-    $url = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href
+    $url64 = $download_page.links | Where-Object href -match $regex | Select-Object -First 1 -expand href | ForEach-Object { 'https://github.com' + $_ }
 
-    $version = ($url -split '/|-' | Select-Object -Last 1 -Skip 3).Substring(1)
+    $version = $url64 -split '-' | Select-Object -Last 1 -Skip 3
     
-    $url64 = 'https://github.com' + $url
-    
-    return @{ URL64 = $url64; Version = $version }
+    @{
+      URL64 = $url64;
+      Version = $version.Replace('v','')
+      ReleaseNotes = "https://github.com/balena-io/balena-cli/releases/tag/${version}"
+    }
 }
 
 update -ChecksumFor none
