9d5b6c627f82c65c2e0126f8d11633aa9c604c6f
jtcmedia <jason.cumiskey@gmail.com>
2020-11-22 08:23:03 -0800

    add handbrake cli pkg

---
+++ handbrake-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  PackageName     = $env:ChocolateyPackageName
+  FileFullPath64  = gi $toolsPath\*-x86_64.zip
+  Destination     = $toolsPath
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+#Don't need installer zip anymore
+rm $toolsPath\*.zip -ea 0 -force
---
+++ handbrake-cli/update.ps1
@@ -0,0 +1,36 @@
+import-module au
+
+$releases = 'https://github.com/HandBrake/HandBrake/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+            "(?i)(\s+checksum64:).*"     = "`${1} $($Latest.Checksum64)"
+            "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = 'x86_64.zip$'
+    $url64 = $download_page.links | ? href -match $regex | select -First 1 -expand href | % { 'https://github.com' + $_ }
+	
+    $version = $url64 -split '-' | select -Last 1 -Skip 2
+    
+    @{
+        URL64 = $url64
+        Version = $version
+        ReleaseNotes = "https://github.com/HandBrake/HandBrake/releases/tag/${version}"
+    }
+}
+
+update -ChecksumFor none
