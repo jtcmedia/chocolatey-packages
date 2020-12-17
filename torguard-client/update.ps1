4054416ef28fe75a53201975ccf57307a194be6c
jtcmedia <jason.cumiskey@gmail.com>
2020-12-17 09:33:41 -0800

    chng version finding statement, inspired from https://github.com/chocolatey-community/chocolatey-coreteampackages/blob/master/automatic/absolute-uninstaller/update.ps1

--- torguard-client/update.ps1
+++ torguard-client/update.ps1
@@ -13,11 +13,11 @@ function global:au_SearchReplace {
 
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
-    $regex = '^v'
-    
-    $version = ($download_page.ParsedHtml.getElementsByTagName("td") | ? InnerHTML -match $regex | select -First 1).InnerText.Substring(1)
+    #$regex = '^v'
+    #$version = ($download_page.ParsedHtml.getElementsByTagName("td") | ? InnerHTML -match $regex | select -First 1).InnerText.Substring(1)
+    $version = ($download_page.Content -split "`n" | sls -Pattern '^<td>v' | select -First 1) -split '<|>' | select -First 1 -Skip 2 | % { $_.Replace('v','') }
     
     $url = "https://torguard.net/downloads/torguard-setup-latest.exe"
     
