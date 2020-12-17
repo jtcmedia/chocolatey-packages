d37a98c0598ac480c92c1fd9774294ef9bf80614
jtcmedia <jason.cumiskey@gmail.com>
2020-12-16 18:44:08 -0800

    must install under users path to work

--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -3,9 +3,11 @@ $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
 $url32      = 'https://cdn.cypress.io/desktop/6.1.0/win32-x64/cypress.zip'
 $checksum32 = 'f72cb5458531850930652b2a66a78d955ecccef9d5bd2c39de15149ae933d708'
 
+$localAppData = [Environment]::GetFolderPath("LocalApplicationData")
+
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
-  UnzipLocation   = $toolsPath
+  UnzipLocation   = $localAppData
   Url             = $url32
   Checksum        = $checksum32
   ChecksumType    = 'sha256'
@@ -17,7 +19,6 @@ $desktopPath = [Environment]::GetFolderPath("Desktop")
 
 Install-ChocolateyShortcut `
   -ShortcutFilePath "$desktopPath\Cypress.lnk" `
-  -TargetPath "$env:ChocolateyInstall\bin\Cypress.exe" `
-  -WorkingDirectory "$toolsPath"
+  -TargetPath "$localAppData\Cypress\Cypress.exe"
 
 rm $toolsPath\*.zip -ea 0
