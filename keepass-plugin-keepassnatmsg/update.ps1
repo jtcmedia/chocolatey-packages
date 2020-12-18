adfe7c2650934eac598e0f9adc58ddd9d705f74d
jtcmedia <jason.cumiskey@gmail.com>
2020-12-18 15:57:05 -0800

    streamline update script more inline w/others from github

--- keepass-plugin-keepassnatmsg/update.ps1
+++ keepass-plugin-keepassnatmsg/update.ps1
@@ -1,32 +1,33 @@
 import-module au
 
-$releases    = 'https://github.com/smorks/keepassnatmsg/releases'
+$releases    = 'https://github.com/smorks/keepassnatmsg/releases/latest'
 
 function global:au_SearchReplace {
     @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
         ".\tools\chocolateyInstall.ps1" = @{
             '(^\s*url\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
-function global:au_BeforeUpdate() {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-    $Latest.ChecksumType32 = 'SHA256'
-}
-
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regexUrl = "KeePassNatMsg-v(?<version>.*)-binaries.zip"
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = '.zip$'
+    $url =  $download_page.links | ? href -match $regex | select -First 1 -expand href | % { 'https://github.com' + $_ }
 
-    $url =  $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
+    $version = $url -split '-' | select -First 1 -Skip 1
 
     return @{
-        URL32        = 'https://github.com'+ $url
-        Version      = $matches.version
+        URL32        = $url
+        Version      = $version.Replace('v','')
+        ReleaseNotes = "https://github.com/smorks/keepassnatmsg/releases/tag/${version}"
     }
 }
 
-update -ChecksumFor none
+update -ChecksumFor 32
