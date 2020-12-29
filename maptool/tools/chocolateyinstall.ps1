920761370b5584c88b6b38345cd8441c6d2e9f54
jtcmedia <jason.cumiskey@gmail.com>
2020-12-29 10:14:15 -0800

    add maptool

---
+++ maptool/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
+$url32      = 'https://github.com/RPTools/maptool/releases/download/1.7.0/MapTool-1.7.0.exe'
+$checksum32 = '1b3bf7345919a4e5b7d6f4cbe4515f0a419a1e114046557063a25ea1b08a10de'
+
+$packageArgs = @{
+  PackageName     = $env:ChocolateyPackageName
+  FileType        = 'EXE'
+  Url             = $url32
+  Checksum        = $checksum32
+  ChecksumType    = 'sha256'
+  SilentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  ValidExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+# remove installer exe
+rm $toolsPath\*.exe -ea 0
---
+++ maptool/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+$releases = 'https://github.com/RPTools/maptool/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
+        ".\tools\chocolateyinstall.ps1" = @{
+          "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+          "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    
+    $regex = '.exe$'
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href | % { 'https://github.com' + $_ }
+
+    $version = $url -split '/' | select -Last 1 -Skip 1
+    
+    @{
+        URL32 = $url
+        Version = $version
+        ReleaseNotes = "https://github.com/RPTools/maptool/releases/tag/${version}"
+    }
+}
+
+update -ChecksumFor 32
