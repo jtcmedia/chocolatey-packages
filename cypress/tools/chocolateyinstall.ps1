7c0ac300326e06e8de36a5fcc7fe64afc0cfd4c6
Chocolatey <chocolatey@realdimensions.net>
2021-03-01 19:41:12 +0000

    AU: 1 updated - cypress [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/4604788826f593bede2b38f931eb994938fc8177

--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url32      = 'https://cdn.cypress.io/desktop/6.5.0/win32-ia32/cypress.zip'
-$checksum32 = '626fcd412b35feeb9d59bc1b3bd05e94a59d42ec5507d64132e58cf9d9e871d8'
-$url64      = 'https://cdn.cypress.io/desktop/6.5.0/win32-x64/cypress.zip'
-$checksum64 = '0b2bcf3a88b85ef45c260d69dee2a543e32236c3925ce060ee82196f2af0d0a5'
+$url32      = 'https://cdn.cypress.io/desktop/6.6.0/win32-ia32/cypress.zip'
+$checksum32 = 'e3bedd93027c14c4058b24b4c9af159675a0b91e056f54ea364ba0d1abb8f590'
+$url64      = 'https://cdn.cypress.io/desktop/6.6.0/win32-x64/cypress.zip'
+$checksum64 = 'dc500910e10ad7bcb396484c77c775b17d511c4cafa52f6f07adb59f09d697f7'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
