292b3c9b73cbf8f788a35801098ea48cdedf92f1
Chocolatey <chocolatey@realdimensions.net>
2021-03-15 03:10:19 +0000

    AU: 1 updated - opentrack [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/90d62d0434090bb4e8b68e30b8a0d7db1a78e13f

--- opentrack/tools/chocolateyinstall.ps1
+++ opentrack/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32      = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2.3.12/opentrack-2.3.12-win32-setup.exe'
-$checksum32 = '0b92b3ae911d88c7be056f296fe937df73a9b36d306f8c498948dd29c5ffd447'
+$url32      = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2.3.13/opentrack-2.3.13-win32-setup.exe'
+$checksum32 = '41b43443f70d8d1ffb77db3a17ecbbb308fab2a9cade87c387c37604081712f8'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
