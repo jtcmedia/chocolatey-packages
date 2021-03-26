7b69702f0d72c864d95ce959ae63e88ceba96a39
Chocolatey <chocolatey@realdimensions.net>
2021-03-26 03:10:14 +0000

    AU: 2 updated - balena-cli unity-docs [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/8e91b5701a79c690fda07994e7e974e250d2f9e5

--- unity-docs/tools/chocolateyinstall.ps1
+++ unity-docs/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-docs'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/77a89f25062f/WindowsDocumentationInstaller/UnityDocumentationSetup.exe'
-$checksum64     = '83efeea1e073019854b6faacac599f345bc1b1f0d6747dbf9e22fbe0f545c81b'
+$url64          = 'https://download.unity3d.com/download_unity/61a549675243/WindowsDocumentationInstaller/UnityDocumentationSetup.exe'
+$checksum64     = '5072d3f1eb49fd0a2260a003e4880566b6c2076bcd03142562a08eaa5ab640a5'
 
 $packageArgs = @{
   packageName    = $packageName
