3d56980a57233873dc2e91f4ac95c43025248fb2
Chocolatey <chocolatey@realdimensions.net>
2021-03-29 03:10:11 +0000

    AU: 1 updated - staxrip [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/69195f426c73aed8ed2b80fbc3bbd4845becc284

--- staxrip/tools/chocolateyinstall.ps1
+++ staxrip/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://github.com/staxrip/staxrip/releases/download/v2.2.0/StaxRip-v2.2.0-x64.7z'
-$checksum64 = '93ecb2a73319a1fed8267bd4bf64601e3cc70980458b439561ff6d77f7453fad'
+$url64      = 'https://github.com/staxrip/staxrip/releases/download/v2.3.0/StaxRip-v2.3.0-x64.7z'
+$checksum64 = '0eb1f65095055ef8039005df61b800f62a3952bbd86727499b8fb962b6d0fc4a'
 
 $unzipLocation = New-Item -Path $(Get-ToolsLocation) -Name "StaxRip" -ItemType "directory" -ErrorAction SilentlyContinue -Force
 
