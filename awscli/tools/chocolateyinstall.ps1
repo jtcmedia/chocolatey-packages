6801e1c5ff2b293d96c5ad8a8fcd8945b056dfad
Chocolatey <chocolatey@realdimensions.net>
2021-04-03 03:10:17 +0000

    AU: 4 updated - awscli balena-cli ds4windows unity-ios [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/488804898bd6f8962133f1f99314b2e03addcce1

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.19.42.msi'
-$checksum   = 'c2517cd4fcdf0098bef7cd30ac21a0c1c6101da956c6c960a9b4303710a7955d'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.19.42.msi'
-$checksum64 = 'c3bbce9a6a3e884e39e4d3343a58eb519ce56abd05bc526f2b8888b85937871a'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.19.44.msi'
+$checksum   = 'c8c6c6773b1ca20479054beeca3c809ce3cbe46c5ad9dd69c1f77b74dd67814d'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.19.44.msi'
+$checksum64 = 'f5a872435278a2b706a52cfd0dfb276bf5879eb946ded8b58239967f4367e2bf'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- unity-ios/tools/chocolateyinstall.ps1
+++ unity-ios/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'unity-ios'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/61a549675243/TargetSupportInstaller/UnitySetup-iOS-Support-for-Editor-2021.1.0f1.exe'
-$checksum64     = 'ccad11bec422d46d08b9856558cd6b23f6ff7a1fd1e4bd3e7468462b1dcebb0d'
+$url64          = 'https://download.unity3d.com/download_unity/6fdc41dfa55a/TargetSupportInstaller/UnitySetup-iOS-Support-for-Editor-2021.1.1f1.exe'
+$checksum64     = '0946986e0dd313d1bcaefa9f3f44a42ad755c2eb9ce0c6e23adb7e6c546c15cd'
 
 $packageArgs = @{
   packageName    = $packageName
