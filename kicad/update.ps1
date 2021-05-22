20db578548bc7b8cdc384992b7cc7beb75ebc8ec
jtcmedia <jason.cumiskey@gmail.com>
2021-05-22 09:11:35 -0700

    kicad behind donationware wall; modify updt script to grab actual d/l urls; remove complex versioning scheme for now

--- kicad/update.ps1
+++ kicad/update.ps1
@@ -5,7 +5,7 @@ $releases = 'http://kicad-pcb.org/download/windows/'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
@@ -13,26 +13,38 @@ function global:au_SearchReplace {
     }
 }
 
+function Get-ActualUrl([uri]$url) {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $url
+
+  $path = $download_page.links | ? href -match 'redir.*\.exe$' | select -first 1 -expand href
+
+  return $url.Scheme + "://" + $url.Host + $path
+}
+
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
-    $regex = '.exe$'
-    $urls = $download_page.links | ? href -match $regex | select -First 4 -expand href
+    $regex = 'x86_64\.exe$'
+    $url64 = $download_page.links | ? href -match $regex | select -First 1 -expand href
 	
-    $version = ($urls[0] -split '-' | select -Last 1 -Skip 1) -replace '_', '.'
+    $version = $url64 -split '-' | select -Last 1 -Skip 1
     
-    $version_length = $version.length
+    # $version_length = $version.length
     
-    if ($version_length -eq 5) {
-      $version = $version + '.00'
-    } elseif ($version_length -eq 7) {
-      $version = $version.PadRight(8,'0')
-    } else {
-      Write-Host 'Invalid version number'
-      return
-    }
+    # if ($version_length -eq 5) {
+    #   $version = $version + '.00'
+    # } elseif ($version_length -eq 7) {
+    #   $version = $version.PadRight(8,'0')
+    # } else {
+    #   Write-Host 'Invalid version number'
+    #   return
+    # }
 
-    return @{ URL = $urls[3]; URL64 = $urls[0]; Version = $version }
+    @{
+      URL32 = Get-ActualUrl $url64.Replace('x86_64','i686')
+      URL64 = Get-ActualUrl $url64
+      Version = $version.Replace('_','.')
+    }
 }
 
 update
