501d79236a38148b27d193c9deb8e1d3e1dacb0f
Chocolatey <chocolatey@realdimensions.net>
2021-05-25 03:10:12 +0000

    AU: 3 updated - awscli balena-cli cypress [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/006547eed7fe2d311331aafeb71790205410c299

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.19.78.msi'
-$checksum   = 'aa0952fdf362aa6d9a440e68bb4c6aa2641b029db06fb5fd9088e58754262baa'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.19.78.msi'
-$checksum64 = 'd246ff12c1b060705aed0c752e68f9e6eab05717e3bc231b536c2e737afd443a'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.19.79.msi'
+$checksum   = 'd8fb280f5ec9ef534d0ca420ddfa07c69f3c9bf3cd2e7a6f8dafefbf0d7c1668'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.19.79.msi'
+$checksum64 = 'a0f7ab9f5e9c550a8518cbef51bf5ee0df34b5fb4ef3b67965a79059bf394ac7'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url32      = 'https://cdn.cypress.io/desktop/7.3.0/win32-ia32/cypress.zip'
-$checksum32 = '78c591a8aeb208cd995a0c4593955546408d4e71d5455bf93511a680f23b69fd'
-$url64      = 'https://cdn.cypress.io/desktop/7.3.0/win32-x64/cypress.zip'
-$checksum64 = '8a8809e4fd22fe7bfc3103c39df3f4fce9db0964450ce927558e9a09558cb26c'
+$url32      = 'https://cdn.cypress.io/desktop/7.4.0/win32-ia32/cypress.zip'
+$checksum32 = '8e3b74434aeee7941ee1332593afad07680b35de68bd3b9876e2613f6364c279'
+$url64      = 'https://cdn.cypress.io/desktop/7.4.0/win32-x64/cypress.zip'
+$checksum64 = 'b7b4d0d530bcfa56fc7298d042b993d47795c2f2db5034f1e6895b1456e0d6fb'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
