fda99634bc65e3845455a5cdd1e1264524f36d66
Chocolatey <chocolatey@realdimensions.net>
2021-09-06 03:06:54 +0000

    AU: 2 updated - opentrack uvtools [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/ee580a207c0d73227b516dcce9eb3ac20b431611

--- opentrack/tools/chocolateyinstall.ps1
+++ opentrack/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32      = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2021.1.1/opentrack-2021.1.1-win32-setup.exe'
-$checksum32 = '4cd1e6cb293ed167a4a0e525315ad93567dce3d4d3dde6fc22e29874801e4247'
+$url32      = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2021.1.2/opentrack-2021.1.2-win32-setup.exe'
+$checksum32 = '8e70bfb3d4ad2d101023d4dbe5a997285616151097cddd894f793478bbe0e02a'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
