1194c368e58a950ac14d5e2a00af762aaa5e541d
Chocolatey <chocolatey@realdimensions.net>
2021-10-26 17:48:43 +0000

    AU: 2 updated - unity unity [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/fdac58191fa68f4673c1a6bb83ccd7c73410c630

--- unity/tools/chocolateyinstall.ps1
+++ unity/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName        = 'unity'
 $toolsDir           = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64              = 'https://download.unity3d.com/download_unity/6f076387c01d/Windows64EditorInstaller/UnitySetup64-2021.1.26f1.exe'
-$checksum64         = 'e60f20df17d0c5b6dad86b6f3cf048862a7f13a0b1ec71933527dfdd1f60633f'
+$url64              = 'https://download.unity3d.com/download_unity/4bf1ec4b23c9/Windows64EditorInstaller/UnitySetup64-2021.2.0f1.exe'
+$checksum64         = '6fda62dfd68347dfd0d07cab1a820a9e00a85dba7f7f33bb9f0c991fd933dd32'
 
 $args = '/S'
 
