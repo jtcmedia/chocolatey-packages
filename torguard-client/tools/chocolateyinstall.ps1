be18106c300bcaa871e68025cd699616007902eb
jtcmedia <jason.cumiskey@gmail.com>
2021-11-10 20:05:41 -0800

    updt->4.8.2

--- torguard-client/tools/chocolateyinstall.ps1
+++ torguard-client/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64      = 'https://torguard.net/downloads/new/torguard-setup-latest.exe'
-$checksum64 = '7cf14e32ee32f93296fc0526c30055e5975a95fb9c99553334553b6ab23278d7'
+$checksum64 = '25c5fe2e7ab7b2aec63aea858224a4ae9eb72f5537ebed66f1edc32b8417bf4f'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
