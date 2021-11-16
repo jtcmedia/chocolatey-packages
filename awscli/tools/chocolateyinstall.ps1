192da1a086e5903a9a3d0a295c4d135b308ebfdd
Chocolatey <chocolatey@realdimensions.net>
2021-11-16 03:10:15 +0000

    AU: 2 updated - awscli t-rex [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/9d68b91545eed7faa1a2024a78f4af8296c2fb79

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.22.5.msi'
-$checksum   = 'c5459e082b294cdec5cab71b88108e4dd266507d375fc1c3429c28ad3a1fe50e'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.22.5.msi'
-$checksum64 = '0d314aaf11d4c6e57f43e1c52d4c218751b9baec61bd73e7b9804f07c7da6bd5'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.22.6.msi'
+$checksum   = 'ea353282d8c4940d76d8bc9aa877decd53773cd7f61437364d5354078b0e9454'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.22.6.msi'
+$checksum64 = '37677ed308c04069f2fb509356fe1190133108ca031b3634f19c5f5f87f29e52'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- t-rex/tools/chocolateyinstall.ps1
+++ t-rex/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url32      = 'https://github.com/trexminer/T-Rex/releases/download/0.24.6/t-rex-0.24.6-win.zip'
-$checksum32 = '6c79a4395269afee2c9ee97c78edaa395dca973bdf68a69eb14618f0db1c36bd'
+$url32      = 'https://github.com/trexminer/T-Rex/releases/download/0.24.7/t-rex-0.24.7-win.zip'
+$checksum32 = 'c0e508608dd3a26ce60e03320159b80de474167a044a1a572d9d9756c71f6aa0'
 
 $unzipLocation = New-Item -Path $(Get-ToolsLocation) -Name "T-Rex" -ItemType "directory" -ErrorAction SilentlyContinue -Force
 
