8844dabdbf16057314c039614d354a6455b6b578
jtcmedia <jason.cumiskey@gmail.com>
2021-11-25 19:39:02 -0800

    typora d/l urls changed; release fixed version

--- typora/tools/chocolateyinstall.ps1
+++ typora/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://typora.io/windows/typora-update-ia32-1117.exe'
+$url        = 'https://download.typora.io/windows/typora-update-ia32-1117.exe'
 $checksum   = '4784a2fa4e4c1918647cf12f3945c70dc281aa2f587326af7e16fb4c5f981915'
-$url64      = 'https://typora.io/windows/typora-update-x64-1117.exe'
+$url64      = 'https://download.typora.io/windows/typora-update-x64-1117.exe'
 $checksum64 = '51ddc37ddac94eeb0d92d0fef6fea02cf9339628472c9ddf7c3e13a7694be738'
 
 $packageArgs = @{
