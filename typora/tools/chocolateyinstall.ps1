1225069fff2c6e770bfe2a0c4ce739b0a0fc59c5
jtcmedia <jason.cumiskey@gmail.com>
2021-11-28 11:07:44 -0800

    add trial tag; updt to 1.0.2

--- typora/tools/chocolateyinstall.ps1
+++ typora/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.typora.io/windows/typora-update-ia32-1117.exe'
-$checksum   = '4784a2fa4e4c1918647cf12f3945c70dc281aa2f587326af7e16fb4c5f981915'
-$url64      = 'https://download.typora.io/windows/typora-update-x64-1117.exe'
-$checksum64 = '51ddc37ddac94eeb0d92d0fef6fea02cf9339628472c9ddf7c3e13a7694be738'
+$url        = 'https://download.typora.io/windows/typora-setup-ia32-1.0.2.exe'
+$checksum   = 'd35302bf52a1673c552f347c4be8854f98694a7f3719ee24e3812f7d85daea31'
+$url64      = 'https://download.typora.io/windows/typora-setup-x64-1.0.2.exe'
+$checksum64 = 'e1a1175548b042fe6c6e21e74a7b7f9472b8e6c485838ecd4cbc9899a9c7bd21'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
