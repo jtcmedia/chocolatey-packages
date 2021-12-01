aa2b324b9847c65547d0c4bdd78824d0c9055326
Chocolatey <chocolatey@realdimensions.net>
2021-12-01 03:06:42 +0000

    AU: 2 updated - balena-cli maptool [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/31597e75c6aaf54ec7f5a506caabdb0c21d5b857

--- maptool/tools/chocolateyinstall.ps1
+++ maptool/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url32      = 'https://github.com/RPTools/maptool/releases/download/1.10.4/MapTool-1.10.4.msi'
-$checksum32 = '38855a5857e48d64a2bfb4e8a0af91dfcab7f6c83a4a2c591166c17a3355b46c'
+$url32      = 'https://github.com/RPTools/maptool/releases/download/1.11.0/MapTool-1.11.0.msi'
+$checksum32 = '7baf5bc4f756422889554cffe569eb7a6d87c3810c95eebc07f99b73f7616dc6'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
