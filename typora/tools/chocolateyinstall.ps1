9527d82fcb21fab4ea0c7226fdf65431e4db1750
jtcmedia <jason.cumiskey@gmail.com>
2021-12-06 08:04:03 -0800

    checksum changed; updt pkg

--- typora/tools/chocolateyinstall.ps1
+++ typora/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://download.typora.io/windows/typora-setup-ia32-1.0.3.exe'
 $checksum   = 'b8fef97119001c768df262b617d0ea636c45016854013963c213a15dcd948d93'
 $url64      = 'https://download.typora.io/windows/typora-setup-x64-1.0.3.exe'
-$checksum64 = 'bf0cad192a59b6fb1df04dc2d716f98408be6b6ba4a97ee0b9823613eeb1246b'
+$checksum64 = 'ac11151e906f78f79294f6f7656ac67426ac0de95a322eefffe15cbde502206c'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
