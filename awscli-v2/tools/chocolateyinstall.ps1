1058d9d916ddae04e4bbbe0e7999150918a091a9
Chocolatey <chocolatey@realdimensions.net>
2022-02-09 03:10:30 +0000

    AU: 2 updated - awscli nodejs-lts [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/39ecf7513f7a6f8407935c0506a1726ac5cd699c

--- awscli-v2/tools/chocolateyinstall.ps1
+++ awscli-v2/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.4.16.msi'
-$checksum64 = '192ca3e123b5f3fe0eb2f883c79f70a47c0904ac1dbe6e1abe32d493b74c6259'
+$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.4.17.msi'
+$checksum64 = '06c985213d3b848bed3534fb46d8a1d570470b130cf6916dd03a7dca50280162'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
