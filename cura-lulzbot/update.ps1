427d1963db0a569599c406e8c59c1a8aedac68b2
jtcmedia <jason.cumiskey@gmail.com>
2022-03-11 10:29:45 -0800

    fix updt script to reflect change in filename, closes #55

--- cura-lulzbot/update.ps1
+++ cura-lulzbot/update.ps1
@@ -17,14 +17,8 @@ function global:au_GetLatest {
     $regex = '.exe$'
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
 	
-    $version = $url -split '-' | select -Last 2 -Skip 1
+    $version = $url -split '-' | select -Last 1 -Skip 2
 	
-    if($version[0] -match '^lulzbot$') {
-      $version = $version[1]
-    } else {
-      $version = $version[0] + '.' + $version[1]
-    }
-
     @{
         URL = $url
         Version = $version
