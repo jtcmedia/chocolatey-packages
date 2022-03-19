640975c11d05b382ded955bac58036c2f57a6cf6
jtcmedia <jason.cumiskey@gmail.com>
2022-03-19 11:37:33 -0700

    rename unity-metro to unity-windows-store; deprecate old pkg

--- unity-windows-store/tools/chocolateyinstall.ps1
+++ unity-windows-store/tools/chocolateyinstall.ps1
@@ -1,12 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'unity-metro'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64          = 'https://download.unity3d.com/download_unity/559fc0ec6670/TargetSupportInstaller/UnitySetup-Universal-Windows-Platform-Support-for-Editor-2021.2.16f1.exe'
 $checksum64     = '3def8d9ba18a0bf51c4b4982a7aa7947a6d89d8b9a4eaaa377feae4703efac9f'
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url64
   checksum64     = $checksum64
