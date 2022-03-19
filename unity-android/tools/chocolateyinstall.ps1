fd969fb892f6d96a72dbedbc99d16a0753485b44
jtcmedia <jason.cumiskey@gmail.com>
2022-03-19 12:17:57 -0700

    rm old commented out code

--- unity-android/tools/chocolateyinstall.ps1
+++ unity-android/tools/chocolateyinstall.ps1
@@ -1,12 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'unity-android'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64          = 'https://download.unity3d.com/download_unity/559fc0ec6670/TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-2021.2.16f1.exe'
 $checksum64     = '37e57941366527bb1e03e718b062eb28b7a8a6a60ec2e69bee4d00e81bb7fb1b'
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url64
   checksum64     = $checksum64
