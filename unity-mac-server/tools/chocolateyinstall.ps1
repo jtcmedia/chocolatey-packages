b556bf33a0a497d5d946bab77a2fc662d1f6e740
jtcmedia <jason.cumiskey@gmail.com>
2022-03-19 11:58:52 -0700

    add unity-mac-server component installer

---
+++ unity-mac-server/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64          = 'https://download.unity3d.com/download_unity/559fc0ec6670/TargetSupportInstaller/UnitySetup-Mac-Server-Support-for-Editor-2021.2.16f1.exe'
+$checksum64     = '1a2ac6682d026f2976b2eaca4984a06b64180ae781134b8a2a3dc86426b75e55'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
