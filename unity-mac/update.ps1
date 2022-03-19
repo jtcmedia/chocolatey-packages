eaa90608bca2a3bc3fdf5a355291f17c360ba61d
jtcmedia <jason.cumiskey@gmail.com>
2022-03-19 12:25:34 -0700

    use URLs resolved using unity/update.ps1 and not xml file

--- unity-mac/update.ps1
+++ unity-mac/update.ps1
@@ -1,27 +1,16 @@
-import-module au
-
-$unity_data = Import-CliXml $PSScriptRoot\..\_unity.xml
+. $PSScriptRoot\..\unity\update.ps1
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL_mac_mono)'"
             "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
         }
         
-        ".\unity-mac.nuspec" = @{
-            "(\<dependency id=`"unity`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency id=`"unity`" version=)`"([^`"]+)`"" = "`$1`"`[$($Latest.Version)`]`""
         }
     }
-}
-
-
-function global:au_GetLatest {
-  
-    $url = $unity_data["url"] + "TargetSupportInstaller/UnitySetup-Mac-Mono-Support-for-Editor-" + $unity_data["version"] + "f" + $unity_data["release"]
-    
-    
-    return @{ URL64 = $url -replace 'http:', 'https:'; Version = $unity_data["version"] }
 }
 
 update -ChecksumFor 64
--- unity-win-il2cpp/update.ps1
+++ unity-win-il2cpp/update.ps1
@@ -1,27 +1,16 @@
-import-module au
-
-$unity_data = Import-CliXml $PSScriptRoot\..\_unity.xml
+. $PSScriptRoot\..\unity\update.ps1
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL_windows_il2cpp)'"
             "(^[$]checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
         }
         
-        ".\unity-win-il2cpp.nuspec" = @{
-            "(\<dependency id=`"unity`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<dependency id=`"unity`" version=)`"([^`"]+)`"" = "`$1`"`[$($Latest.Version)`]`""
         }
     }
-}
-
-
-function global:au_GetLatest {
-  
-    $url = $unity_data["url"] + "TargetSupportInstaller/UnitySetup-Windows-IL2CPP-Support-for-Editor-" + $unity_data["version"] + "f" + $unity_data["release"]
-    
-    
-    return @{ URL64 = $url -replace 'http:', 'https:'; Version = $unity_data["version"] }
 }
 
 update -ChecksumFor 64
