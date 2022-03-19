70cba8f29c37aa19e18515650675d67c2dac50b7
jtcmedia <jason.cumiskey@gmail.com>
2022-03-19 11:13:19 -0700

    add unity linux server component installer

---
+++ unity-linux-server/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64          = 'https://download.unity3d.com/download_unity/559fc0ec6670/TargetSupportInstaller/UnitySetup-Linux-Server-Support-for-Editor-2021.2.16f1.exe'
+$checksum64     = 'f043f790f616b0d30f3da94069b892e01a3995bd440c37522336ca695a32a76b'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
