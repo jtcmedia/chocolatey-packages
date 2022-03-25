09d018d32da30cae53ccf3aaccebd3f4c79e5348
Chocolatey <chocolatey@realdimensions.net>
2022-03-25 17:58:47 +0000

    AU: 1 updated - unity-windows-store [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/fd310997717067d948bc57f4ceca22534ed54103

--- unity-windows-store/tools/chocolateyinstall.ps1
+++ unity-windows-store/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64          = 'https://download.unity3d.com/download_unity/019e31cfdb15/TargetSupportInstaller/UnitySetup-Universal-Windows-Platform-Support-for-Editor-2019.4.37f1.exe'
-$checksum64     = 'bcedbf256300d4accadbf2cfff9c1abff5dbc50adbbf0a10e61d5132eb21893c'
+$url64          = 'https://download.unity3d.com/download_unity/efb8f635e7b1/TargetSupportInstaller/UnitySetup-Universal-Windows-Platform-Support-for-Editor-2021.2.17f1.exe'
+$checksum64     = '184a9021c02f64eb299e8c23b77e364cbe37cafa367de788dc7a1bed44081707'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
