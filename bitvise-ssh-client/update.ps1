8d76a01445cf208c0c321baecc93225392e67d50
jtcmedia <jason.cumiskey@gmail.com>
2022-04-18 08:45:28 -0700

    updt Release Notes to reflect 8.x or 9.x version

--- bitvise-ssh-client/update.ps1
+++ bitvise-ssh-client/update.ps1
@@ -8,6 +8,10 @@ function global:au_SearchReplace {
             "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
+
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
     }
 }
 
@@ -20,15 +24,16 @@ function global:au_GetLatest {
     
     if ($download_page.Content -match 'version:\s8\.\d{2}') {
       $version8 = $Matches[0].Substring(9)
+      $releaseNotes8 = 'https://www.bitvise.com/ssh-client-version-history-8'
     } else { return }
 
     $version9 = $urls[2] -split '-|\.exe' | select -Last 1 -Skip 1
-
+    $releaseNotes9 = 'https://www.bitvise.com/ssh-client-version-history'
 
     @{
         Streams = [ordered] @{
-          '8' = @{ Version = $version8; URL32 = $urls[0] }
-          '9' = @{ Version = $version9.Insert(1, '.'); URL32 = $urls[2] }
+          '8' = @{ Version = $version8; URL32 = $urls[0]; ReleaseNotes = $releaseNotes8 }
+          '9' = @{ Version = $version9.Insert(1, '.'); URL32 = $urls[2]; ReleaseNotes = $releaseNotes9 }
         }
     }
 }
