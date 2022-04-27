b8a650735c6ba93660dd4b4493bd399d1f710eb1
jtcmedia <jason.cumiskey@gmail.com>
2022-04-27 15:00:58 -0700

    add remix ide desktop pkg

---
+++ remix-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file64        = gi $toolsDir\*.exe
+  softwareName  = 'Remix IDE*'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+rm $toolsDir\*.exe -ea 0 #remove installer exe
