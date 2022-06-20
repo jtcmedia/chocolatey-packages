cecec863652be49992480979abc40841142b0c23
jtcmedia <jtcmedia+github@gmail.com>
2022-06-20 03:56:17 +0000

    AU: 2 updated - opentrack uvtools [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/b5750b9ca93aefa66d2a40a3d0120b44f7210526

--- opentrack/tools/chocolateyinstall.ps1
+++ opentrack/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32      = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2022.2.0/opentrack-2022.2.0-win32-setup.exe'
-$checksum32 = 'fd08d60c4f3109c205a415cb9bda00cb782dbee0460a1083da7f8674a10b9efe'
+$url32      = 'https://github.com/opentrack/opentrack/releases/download/opentrack-2022.3.0/opentrack-2022.3.0-win32-setup.exe'
+$checksum32 = '83559a33044ee27a82a627965e8551f08514219e40653e5aa3201040f3e9c136'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
