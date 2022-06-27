3c62db0e43b0b4c3d4aa6359ec1c73e3258a117c
jtcmedia <jtcmedia+github@gmail.com>
2022-06-27 04:21:16 +0000

    AU: 1 updated - kicad [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/7bd4e661d9137fee94bba0545078f7426b69471b

--- kicad/tools/chocolateyinstall.ps1
+++ kicad/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://osdn.net/frs/redir.php?m=rwthaachen&f=%2Fstorage%2Fg%2Fk%2Fki%2Fkicad%2Fkicad-6.0.6-i686.exe'
-$url64      = 'https://osdn.net/frs/redir.php?m=rwthaachen&f=%2Fstorage%2Fg%2Fk%2Fki%2Fkicad%2Fkicad-6.0.6-x86_64.exe'
-$checksum32 = '335d4c71d62893c477f051d4230127741d5fb7f08f8ec93896858ef354d9ab5a'
-$checksum64 = 'cc7271edd6ce14b2bd44429fa3d9c188b7ed7ab067891f491763ab1073dea0f3'
+$url32      = 'https://osdn.net/frs/redir.php?m=rwthaachen&f=%2Fstorage%2Fg%2Fk%2Fki%2Fkicad%2Fkicad-6.0.6_1-i686.exe'
+$url64      = 'https://osdn.net/frs/redir.php?m=nchc&f=%2Fstorage%2Fg%2Fk%2Fki%2Fkicad%2Fkicad-6.0.6_1-x86_64.exe'
+$checksum32 = 'e2db52d6ef4100bf30da471ab4f4e85767d8fb130abc994137cfa472874e80b7'
+$checksum64 = '00764eecb9a7ebde5c5635b16cd5d009934102d5bea68a64afb18f7f41aef4e4'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
