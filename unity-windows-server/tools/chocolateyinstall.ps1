e75b60ab38dd2591a52b75295b7e6fa2b5c34435
jtcmedia <jtcmedia+github@gmail.com>
2022-07-11 04:11:17 +0000

    AU: 3 updated - ds4windows tesseract-ocr unity-windows-server [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/8890cc445802eeb93da58e5e6a7386373a6811c8

--- unity-windows-server/tools/chocolateyinstall.ps1
+++ unity-windows-server/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64          = 'https://download.unity3d.com/download_unity/2961e8c2b463/TargetSupportInstaller/UnitySetup-Windows-Server-Support-for-Editor-2022.1.8f1.exe'
-$checksum64     = 'c4009e0e4355954edc5a8cf45e5a507ce21528c05e425d7cb8f93292b8aeb42d'
+$url64          = 'https://download.unity3d.com/download_unity/7da38d85baf6/TargetSupportInstaller/UnitySetup-Windows-Server-Support-for-Editor-2021.3.6f1.exe'
+$checksum64     = 'f29afe82135110b9ba5f65f77e854ea03bf213fcb68641f3ea5f920f1589c18f'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
