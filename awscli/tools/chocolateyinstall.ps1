e62b16706baaab34d35387c6259d4e04dba6be61
jtcmedia <jtcmedia+github@gmail.com>
2022-08-18 04:31:42 +0000

    AU: 4 updated - argocd-cli awscli balena-cli uvtools [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/0626e066310b6857b82bd6478e86992d941d74f5

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.25.51.msi'
-$checksum   = '4341adeb2957c087289348c7f1c136aa791b1dce5e413505027a2288b8aa9769'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.25.51.msi'
-$checksum64 = '9314f2b56c517582725ccfac125972a79095b4b3941e72f93963e162316a2735'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.25.55.msi'
+$checksum   = '6b8234200c3a12199673062ef6b292c71673a25a52ace9a996d68832c04f277d'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.25.55.msi'
+$checksum64 = '62d737b74c181128d9bd371a489dfec0717bf5808bce6359628b172ced5f8481'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
