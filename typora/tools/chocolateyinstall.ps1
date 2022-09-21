2be19b86c1c33644e25501e47abc8d180b8cffb7
jtcmedia <jtcmedia+github@gmail.com>
2022-09-21 05:06:00 +0000

    AU: 1 updated - typora [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/7908ed866bd29b6491359bcd04c6575dc80a6a95

--- typora/tools/chocolateyinstall.ps1
+++ typora/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.typora.io/windows/typora-setup-ia32-1.4.4.exe'
-$checksum   = 'c1300ac9705fae71d09805c26509f85057118b31b5dce255ddcd7ba31a96f70e'
-$url64      = 'https://download.typora.io/windows/typora-setup-x64-1.4.4.exe'
-$checksum64 = '9d015818bf789306511ca7091e18afb5105accce2b3f4090b455345a60094c3a'
+$url        = 'https://download.typora.io/windows/typora-setup-x64-1.4.4.exe'
+$checksum   = '9d015818bf789306511ca7091e18afb5105accce2b3f4090b455345a60094c3a'
+$url64      = 'https://download.typora.io/windows/typora-setup-x64-1.4.7.exe'
+$checksum64 = 'e7a513ae0e00f28eed10aa8001b18fb03ea8b4258e9c6dae01f95d18081d9611'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
