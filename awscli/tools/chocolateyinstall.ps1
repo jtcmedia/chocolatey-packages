bfdc0cb2c0d2289f3663bd85629cae01c3fe079c
jtcmedia <jtcmedia+github@gmail.com>
2022-10-05 04:12:20 +0000

    AU: 1 updated - awscli [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/9fdeac57c1a8ee81ef9430a29c65095ef759caf6

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.25.83.msi'
-$checksum   = '8df9e1273656a7eb8bd9fb822f4fb6c14fd2381b7fc0c41d8014ffb45d57f4c6'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.25.83.msi'
-$checksum64 = 'ae1af1d468e4a69d7bdf2e22d90d4ae62f58ce4bb50e108acc44d9999632556b'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.25.87.msi'
+$checksum   = '119509de37fa99c2e10b33dee1778aec28017705ef61b7f450224a9ec0ae999c'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.25.87.msi'
+$checksum64 = 'fbe361746d0e7c8403170c0c42d37354d42129ba76f4205eea67f80c41204a15'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
