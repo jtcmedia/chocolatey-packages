c14c2c3c1b84bf7864c0c95ec727be7616095237
jtcmedia <jason.cumiskey@gmail.com>
2022-10-28 09:20:33 -0700

    github updt scripts have stopped working; script courtesy of TheCakeIsNaOH; requires Wormies AU Helpers

---
+++ helper-scripts/Get-GitHubLatestReleaseLinks.ps1
@@ -0,0 +1,23 @@
+# courtesy of TheCakeIsNaOH
+# see https://discordapp.com/channels/778552361454141460/1019078061427265546/1027736480195559466
+
+function Get-GitHubLatestReleaseLinks() {
+  param(
+    [parameter(Mandatory=$true)][string]$user,
+    [parameter(Mandatory=$true)][string]$repository,
+    [switch]$absolutelatestrelease
+  )
+  
+  if ($absolutelatestrelease) {
+    $releases_url = "https://github.com/" + $user + "/" + $repository + "/releases"
+    $releases_page = Invoke-WebRequest -Uri $releases_url -UseBasicParsing
+    $release_tag_url = "https://github.com" + ($releases_page.links | Where-Object href -match "/tag/v" | Select-Object -First 1 -ExpandProperty href)
+  } else {
+    $latest_releases_url = "https://github.com/" + $user + "/" + $repository + "/releases/latest"
+    $release_tag_url = Get-RedirectedUrl -url $latest_releases_url
+  }
+
+  $expanded_assets_url = $release_tag_url -replace "/tag/","/expanded_assets/"
+  $assets_page = Invoke-WebRequest -Uri $expanded_assets_url -UseBasicParsing
+  return $assets_page
+}
