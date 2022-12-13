79eec52b6d04bbf6f547a2f32e6e5a7374391cc4
jtcmedia <jtcmedia+github@gmail.com>
2022-12-13 03:34:57 +0000

    AU: 2 updated - balena-cli cypress [skip ci] https://gist.github.com/8abc37a8c3b020ffdd9704a810331790/ecdf0a22c24a7e17749b020c64dc18931bcc5901

--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://cdn.cypress.io/desktop/12.0.2/win32-x64/cypress.zip'
-$checksum64 = '4b601dc6afb2bf4a942eb7d8f14e0c273253755341c98cd4b463fc5aa5f89741'
+$url64      = 'https://cdn.cypress.io/desktop/12.1.0/win32-x64/cypress.zip'
+$checksum64 = 'd31e044976764478d672b55e671ef564c65b731d74f66e6c173aaa3ccedd5182'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
