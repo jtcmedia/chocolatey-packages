a891a00893e9fd75818d4ad1481d06e228fdba39
jtcmedia <jason.cumiskey@gmail.com>
2023-03-04 22:35:48 -0800

    change to using github mirror site for releases; v7 drops 32 bit release

--- kicad/update.ps1
+++ kicad/update.ps1
@@ -1,50 +1,33 @@
 import-module au
-
-$releases = 'https://www.kicad.org/download/windows/'
+. (Join-Path '..' 'helper-scripts' | Join-Path -ChildPath 'Get-GitHubLatestReleaseLinks.ps1')
 
 function global:au_SearchReplace {
     @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
         ".\tools\chocolateyinstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+          "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+          "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
-function Get-ActualUrl([uri]$url) {
-  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $url
-
-  $path = $download_page.links | ? href -match 'redir.*\.exe$' | select -first 1 -expand href
-
-  return $url.Scheme + "://" + $url.Host + $path
-}
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page = Get-GitHubLatestReleaseLinks -User "KiCad" -Repository "kicad-source-mirror"
     
-    $regex = 'x86_64\.exe$'
-    $url64 = $download_page.links | ? href -match $regex | select -First 1 -expand href
-	
-    $version = $url64 -split '-' | select -Last 1 -Skip 1
-    
-    # $version_length = $version.length
-    
-    # if ($version_length -eq 5) {
-    #   $version = $version + '.00'
-    # } elseif ($version_length -eq 7) {
-    #   $version = $version.PadRight(8,'0')
-    # } else {
-    #   Write-Host 'Invalid version number'
-    #   return
-    # }
+    $regex = '.exe$'
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href | % { 'https://github.com' + $_ }
 
+    $version = $url -split '/' | select -Last 1 -Skip 1
+    
     @{
-      URL32 = Get-ActualUrl $url64.Replace('x86_64','i686')
-      URL64 = Get-ActualUrl $url64
-      Version = $version.Replace('_','.')
+        URL64 = $url
+        Version = $version
+        ReleaseNotes = "https://github.com/KiCad/kicad-source-mirror/releases/tag/${version}"
     }
 }
 
-update
+update -ChecksumFor 64
