24d0964dd99ae88ce7af5a087eaaf2fd34e27971
jtcmedia <jtcmedia+github@gmail.com>
2023-08-02 03:24:00 +0000

    AU: 1 updated - cypress [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/d9f26aed0e87a81da83fe75c1726cec44591f4f0

--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://cdn.cypress.io/desktop/12.17.2/win32-x64/cypress.zip'
-$checksum64 = 'd0f9741a1720f889129b9d2d0fa089f746c6774af619dcd7d85d8a8a7b3964dc'
+$url64      = 'https://cdn.cypress.io/desktop/12.17.3/win32-x64/cypress.zip'
+$checksum64 = 'b61fc78dc8898137bd43d2fbfe22cacb2cbf512adc971a9858d7d4a3a2b83a0c'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
