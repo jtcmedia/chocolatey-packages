6884cfefd6b37be9257f018194f512fb51bc3303
jtcmedia <jason.cumiskey@gmail.com>
2023-10-24 11:00:09 -0700

    no longer releasing 32 bit version; minor fixes to updt script

--- tesseract-ocr/update.ps1
+++ tesseract-ocr/update.ps1
@@ -5,10 +5,10 @@ $releases = 'https://github.com/UB-Mannheim/tesseract/wiki'
 function global:au_SearchReplace {
     @{
         ".\tools\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"                   = "`${1} $($Latest.URL32)"
+          #"(?i)(\s+x32:).*"                   = "`${1} $($Latest.URL32)"
           "(?i)(\s+x64:).*"                   = "`${1} $($Latest.URL64)"
           "(?i)(Get-RemoteChecksum).*"        = "`${1} $($Latest.URL64)"
-          "(?i)(\s+checksum32:).*"            = "`${1} $($Latest.Checksum32)"
+          #"(?i)(\s+checksum32:).*"            = "`${1} $($Latest.Checksum32)"
           "(?i)(\s+checksum64:).*"            = "`${1} $($Latest.Checksum64)"
         }
     }
@@ -21,14 +21,14 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
     $regex = '.exe$'
-    $urls = $download_page.links | ? href -match $regex | select -First 2 -expand href
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
 
-    $version = $urls[1] -split '-|.exe' | select -Last 1 -Skip 1 | % { $_.Replace('v','') }
-    #$alpha = $urls[1] -split '-|.exe' | select -Last 1 -Skip 1
+    $version = $url -split '-|.exe' | select -Last 1 -Skip 1
+    #$version = $urls[1] -split '-|.exe' | select -Last 1 -Skip 1 | % { $_.Replace('v','') }
     
     @{
-        URL32 = $urls[0]
-        URL64 = $urls[1]
+        #URL32 = $urls[0]
+        URL64 = $url
         #Version = "$($version.Replace('v',''))-$($alpha.Replace('.',''))"
         Version = $version
     }
