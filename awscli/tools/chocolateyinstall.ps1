3f4270c8b4e2610bbfa17a2005e5afcc80b41724
jtcmedia <jtcmedia+github@gmail.com>
2023-10-27 03:23:34 +0000

    AU: 2 updated - awscli balena-cli [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/0b79efecf7550f9b4eb4caf16b32bb527aaefc25

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.29.70.msi'
-$checksum   = ''
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.29.70.msi'
-$checksum64 = ''
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.29.72.msi'
+$checksum   = '30ffcc8be4f50e74f7189d2083d5c064b61361c2bddda44c749451efdd99e35f'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.29.72.msi'
+$checksum64 = '1578a7f0cf0741cbb0117927e1fbd784a2dc0ca42e36c39803e1c51c302d038c'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
