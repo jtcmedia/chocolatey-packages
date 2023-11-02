018c4f240acb88cd8ea296c571fb118a71f93748
jtcmedia <jason.cumiskey@gmail.com>
2023-11-01 23:06:44 -0700

    fix broken updt script by adding user-agent, closes #73

--- torguard-client/update.ps1
+++ torguard-client/update.ps1
@@ -19,14 +19,14 @@ function global:au_SearchReplace {
 
 
 function global:au_GetLatest {
- 
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $download_page = Invoke-WebRequest -Uri $releases -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
     
     #$regex = '^v'
     #$version = ($download_page.ParsedHtml.getElementsByTagName("td") | ? InnerHTML -match $regex | select -First 1).InnerText.Substring(1)
     $version = ($download_page.Content -split "`n" | sls -Pattern '^<td>v' | select -First 1) -split '<|>' | select -First 1 -Skip 2 | % { $_.Replace('v','') }
     
-    $url = "https://torguard.net/downloads/new/torguard-setup-latest.exe"
+    $url = "https://updates.torguard.biz/Software/Windows/torguard-setup-latest.exe"
 
     $regex = 'forums'
     $release_notes = $download_page.links | ? href -match $regex | select -First 1 -expand href
