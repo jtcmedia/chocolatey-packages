34d560148aded352da594fe121202a06232b5567
jtcmedia <jason.cumiskey@gmail.com>
2023-12-12 20:18:11 -0800

    use BypassCloudflare script

--- torguard-client/update.ps1
+++ torguard-client/update.ps1
@@ -1,4 +1,5 @@
 import-module au
+. (Join-Path '..' 'helper-scripts' | Join-Path -ChildPath 'ByPassCloudflare.ps1')
 
 $releases = 'https://torguard.net/downloads.php'
 $checksums = 'https://updates.torguard.biz/Software/Windows/checksums.sha256'
@@ -20,15 +21,17 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  
-    $download_page = Invoke-WebRequest -Uri $releases -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
+    $download_page = (ByPassCloudFlare -Url $releases | ConvertFrom-Json).solution.response
     
     #$regex = '^v'
     #$version = ($download_page.ParsedHtml.getElementsByTagName("td") | ? InnerHTML -match $regex | select -First 1).InnerText.Substring(1)
-    $version = ($download_page.Content -split "`n" | sls -Pattern '^<td>v' | select -First 1) -split '<|>' | select -First 1 -Skip 2 | % { $_.Replace('v','') }
+    $version = ($download_page -split "`n" | sls -Pattern '^<td>v' | select -First 1) -split '<|>' | select -First 1 -Skip 2 | % { $_.Replace('v','') }
     
     $url = "https://updates.torguard.biz/Software/Windows/torguard-setup-latest.exe"
+
+    $download_checksums = (ByPassCloudFlare -Url $checksums | ConvertFrom-Json).solution.response
+    
+    $checksum = ($download_checksums -split "`n" | select -Last 1 -Skip 1) -split '\s{2}' | select -First 1
 
     $regex = 'forums'
     $release_notes = $download_page.links | ? href -match $regex | select -First 1 -expand href
@@ -37,8 +40,9 @@ function global:au_GetLatest {
         URL64 = $url
         Version = $version
         ReleaseNotes = $release_notes
+        Checksum64 = $checksum
     }
 
 }
 
-update -ChecksumFor 64
+update -ChecksumFor none
