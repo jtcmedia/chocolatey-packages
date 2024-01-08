7b069ff46d63609c973fb174eef370bb9433b7b1
jtcmedia <jtcmedia+github@gmail.com>
2024-01-08 15:51:36 +0000

    AU: 2 updated - dxwnd staxrip [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/36672e9b1cfd631321e0d4cc2191a45d65148a9d

--- dxwnd/tools/chocolateyinstall.ps1
+++ dxwnd/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url32      = 'https://sourceforge.net/projects/dxwnd/files/Latest%20build/V2_06_00_build.rar/download'
-$checksum32 = 'c3221d2567cac0e7d6c4d017121895bd08f3505ad8d60cbb92eaadbaecde92e3'
+$url32      = 'https://sourceforge.net/projects/dxwnd/files/Latest%20build/v2_06_01_build.rar/download'
+$checksum32 = 'd9abb9976752483a94c3123f4de6e23c409efb7dae9b14a23e2f748b893971b6'
 
 $unzipLocation = New-Item -Path $(Get-ToolsLocation) -Name "DxWnd" -ItemType "directory" -ErrorAction SilentlyContinue -Force
 
