318240650d5ad69f3096619823a596a03712de88
jtcmedia <jtcmedia+github@gmail.com>
2024-01-17 03:25:51 +0000

    AU: 3 updated - arduino-cli awscli cypress [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/0c61fe476448aafbdd4b3c50047b6cd8bc451d2a

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.32.19.msi'
-$checksum   = 'de58659fc12f303329731c9f3d1ffdb32f378f87ab2148a56cd31bbd5574ed0a'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.32.19.msi'
-$checksum64 = '9d08bb49eef6224fc2ccf9027b539c0084c7c2248cf9e94de4f42ab621f2c2b7'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.32.20.msi'
+$checksum   = '387a5b8311c955988c44838c5480150776a38c32a87d9fea9803a36688145daa'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.32.20.msi'
+$checksum64 = 'e744ad86664200e9958d9d120b23b2add73a4000fbd6a2349de6ba88d250135a'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://cdn.cypress.io/desktop/13.6.2/win32-x64/cypress.zip'
-$checksum64 = '858e1f4a981c464df28acf7d788d0e37b23368914a16ca1a8f6e0391f4934357'
+$url64      = 'https://cdn.cypress.io/desktop/13.6.3/win32-x64/cypress.zip'
+$checksum64 = '77432255bb784079baa3652ca69533e433b5c7b503218af81c9f08104c9cc30f'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
