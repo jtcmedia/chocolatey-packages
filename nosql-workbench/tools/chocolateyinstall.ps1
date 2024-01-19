6febaa73620c7db73b64bb98b51f6d43e24ca2e3
jtcmedia <jason.cumiskey@gmail.com>
2024-01-18 21:26:14 -0800

    Using NSIS again, change silentArgs back to /S

--- nosql-workbench/tools/chocolateyinstall.ps1
+++ nosql-workbench/tools/chocolateyinstall.ps1
@@ -4,14 +4,14 @@ $url64      = 'https://dy9cqqaswpltd.cloudfront.net/NoSQL_Workbench.exe'
 $checksum64 = 'f2655f8adf35b6abc64fac4517563377a4472b8792783c6d185b81a5e66bb58a'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url64bit      = $url64
-  softwareName  = 'NoSQL Workbench*'
-  checksum64    = $checksum64
-  checksumType64= 'sha256'
-  silentArgs   = '--mode unattended --install_dynamoDBLocal 0'
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url64bit       = $url64
+  softwareName   = 'NoSQL Workbench*'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
   validExitCodes= @(0)
 }
 
