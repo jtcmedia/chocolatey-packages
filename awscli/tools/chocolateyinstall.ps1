ee4634bc8b0d3f15a748b8f0fa6fb70c61020abd
jtcmedia <jtcmedia+github@gmail.com>
2024-01-30 03:22:54 +0000

    AU: 3 updated - awscli typora uvtools [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/0f3f01343d3fe3241a17b0ab107710bb1d30eb70

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.32.28.msi'
-$checksum   = '3e2e8928a76a16f35297026b868c4d4f28f855ab453f04c002ba4d0d08146c2e'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.32.28.msi'
-$checksum64 = 'bae9035950f2c6e1e243d54cb854907eaf9baf72042ecfab27066347f8d9f680'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.32.30.msi'
+$checksum   = '3b7571cb211b05156dffd54a1f62a79f8e94dd5c62cc5191ee6bc5a3398e6e82'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.32.30.msi'
+$checksum64 = 'cd29410060f93bf1a1b0ca05252d3eaac548f56b1360cf6b076221a8401d0c77'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- typora/tools/chocolateyinstall.ps1
+++ typora/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.typora.io/windows/typora-setup-ia32-1.8.6.exe'
-$checksum   = 'f55fca6f71e51c23ea602842999fdae9e9d90631e3218af9d976784319d721dd'
-$url64      = 'https://download.typora.io/windows/typora-setup-x64-1.8.6.exe'
-$checksum64 = '66b439b76771eb02d11f186a1994842959f1db2921ed465efd9b83a301859912'
+$url        = 'https://download.typora.io/windows/typora-setup-ia32-1.8.9.exe'
+$checksum   = '86bb7ed1157bd1a0d1239f8919c509c3b8bf9ff66a9ff5eb945a447872653142'
+$url64      = 'https://download.typora.io/windows/typora-setup-x64-1.8.9.exe'
+$checksum64 = '5dffa55000750d041dc87a686e1e3f55379ea70805d38d0393d1554f7dd6a892'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
