a9727c7061c6d8d70b653cdcadc0ae61db56732c
jtcmedia <jason.cumiskey@gmail.com>
2024-02-09 09:15:12 -0800

    updt 2.06.02f

--- dxwnd/tools/chocolateyinstall.ps1
+++ dxwnd/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url32      = 'https://sourceforge.net/projects/dxwnd/files/Latest%20build/v2_06_02_build.rar/download'
-$checksum32 = '0638b3d59d22a12971f4491e661228f7b406ad0cd2b167468e0da878e292a7d2'
+$url32      = 'https://sourceforge.net/projects/dxwnd/files/Latest%20build/v2_06_02f_build.rar/download'
+$checksum32 = '4164f910c79900fd3d4f45edcfd83255937e52493e402a7c880ac22d5b9d5203'
 
 $unzipLocation = New-Item -Path $(Get-ToolsLocation) -Name "DxWnd" -ItemType "directory" -ErrorAction SilentlyContinue -Force
 
