e9aa6103f8639a992cd619387a8e8f557e43856d
jtcmedia <jtcmedia+github@gmail.com>
2024-02-21 03:24:24 +0000

    AU: 2 updated - cypress heroku-cli [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/65c89c74047355afaf8fa1b65112e9977a47dbb7

--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://cdn.cypress.io/desktop/13.6.4/win32-x64/cypress.zip'
-$checksum64 = '28e9b5c5225de920522f6e4a7cdbd3125b3d709adf8ba95325b725a304c1cf98'
+$url64      = 'https://cdn.cypress.io/desktop/13.6.5/win32-x64/cypress.zip'
+$checksum64 = '9020b22d6b42e400fe8ba9212b26c015b2734ad1610503b04d39833486419dcf'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
