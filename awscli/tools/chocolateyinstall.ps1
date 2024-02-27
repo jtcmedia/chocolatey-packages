62ee46406702f96b9b425cbfbcae3058eb2a5ce7
jtcmedia <jtcmedia+github@gmail.com>
2024-02-27 03:15:17 +0000

    AU: 2 updated - awscli nosql-workbench [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/b9f53fa559ce9a22e94829c5bbbb0f94dcefd950

--- awscli/tools/chocolateyinstall.ps1
+++ awscli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.32.48.msi'
-$checksum   = '58a295a7671b681a2cf98355370282d2312e100e2825734214c95a1ebb2280c6'
-$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.32.48.msi'
-$checksum64 = '73348207a1273db306d3b58c835bbc1775f02a1a0916c1ccacd4fe34c4e1df74'
+$url        = 'https://s3.amazonaws.com/aws-cli/AWSCLI32PY3-1.32.50.msi'
+$checksum   = '5a17cda998048480146303226f89f8d66051f950e36f3790415ef265f647a916'
+$url64      = 'https://s3.amazonaws.com/aws-cli/AWSCLI64PY3-1.32.50.msi'
+$checksum64 = 'cce73914120d722071c23964e69d11b6946dd934a9e07fa4d44e899e43439f5a'
  
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- nosql-workbench/tools/chocolateyinstall.ps1
+++ nosql-workbench/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = Split-Path $MyInvocation.MyCommand.Definition
 $url64      = 'https://dy9cqqaswpltd.cloudfront.net/NoSQL_Workbench.exe'
-$checksum64 = 'f2655f8adf35b6abc64fac4517563377a4472b8792783c6d185b81a5e66bb58a'
+$checksum64 = 'd1cb031429a5f113e8f8631e55cb235fc32a77b9285a33dca34bdcad8db3a936'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
