9b3e86eb7d226cf13235d5326dfea947dd2aae20
jtcmedia <jason.cumiskey@gmail.com>
2024-04-17 13:01:28 -0700

    add video-compare pkg

---
+++ video-compare/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  PackageName     = $env:ChocolateyPackageName
+  FileFullPath    = gi $toolsPath\*.zip
+  Destination     = $toolsPath
+}
+
+ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
+
+Get-ChocolateyUnzip @packageArgs
+
+rm $toolsPath\*.zip -ea 0
---
+++ video-compare/update.ps1
@@ -0,0 +1,37 @@
+import-module au
+. (Join-Path '..' 'helper-scripts' | Join-Path -ChildPath 'Get-GitHubLatestReleaseLinks.ps1')
+
+function global:au_SearchReplace {
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x64:).*"                   = "`${1} $($Latest.URL64)"
+          "(?i)(Get-RemoteChecksum).*"        = "`${1} $($Latest.URL64)"
+          "(?i)(\s+checksum64:).*"            = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_GetLatest {
+    $download_page = Get-GitHubLatestReleaseLinks -User "pixop" -Repository "video-compare"
+    
+    $regex = '.zip$'
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href | % { 'https://github.com' + $_ }
+
+    $version  = $url -split '/' | select -Last 1 -Skip 1
+    $version = $version.Insert(4, '.').Insert(7, '.')
+
+
+    @{
+        Version = $version
+        URL64 = $url
+        ReleaseNotes = "https://github.com/pixop/video-compare/releases/tag/${version}"
+    }
+}
+
+update -ChecksumFor none
