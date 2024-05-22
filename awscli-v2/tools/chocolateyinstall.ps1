dff776509efddadc9da2bf8c358e400563b85ce5
jtcmedia <jtcmedia+github@gmail.com>
2024-05-22 01:36:31 +0000

    AU: 4 updated - argocd-cli awscli cypress nodejs-lts [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/5256f071eb7119581694f8f8ae937479272edd7e

--- awscli-v2/tools/chocolateyinstall.ps1
+++ awscli-v2/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.15.54.msi'
-$checksum64 = '05263dcf7ef433e9bcbb74a3261ea9dc107dfd492bfaea79524c32943751de54'
+$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.15.55.msi'
+$checksum64 = 'c754f16bf48473d4d1e6a1f4484d1dfffa6441282163d8b458f7dab5b839c938'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://cdn.cypress.io/desktop/13.9.0/win32-x64/cypress.zip'
-$checksum64 = 'f6bce4c915526010cebabd970800d5af140ea280f1a654b1fe8cb15a25f6eeca'
+$url64      = 'https://cdn.cypress.io/desktop/13.10.0/win32-x64/cypress.zip'
+$checksum64 = '64266573d66c9686001f39eb9c1705afe6becf1b0c94838563a3a1916cfe9220'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
