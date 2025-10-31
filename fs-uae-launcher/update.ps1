97f79f3d5d7576533de19157209eb64a4b1e3d5e
jtcmedia <jtc@jtcmedia.com>
2025-10-30 18:47:25 -0700

    fix updt script

--- fs-uae-launcher/update.ps1
+++ fs-uae-launcher/update.ps1
@@ -1,9 +1,12 @@
 import-module chocolatey-au
-
-$releases = 'https://fs-uae.net/download'
+. (Join-Path '..' 'helper-scripts' | Join-Path -ChildPath 'Get-GitHubLatestReleaseLinks.ps1')
 
 function global:au_SearchReplace {
     @{
+        "$($Latest.PackageName).nuspec" = @{
+          "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+      
         ".\tools\VERIFICATION.txt" = @{
           "(?i)(\s+x64:).*"                   = "`${1} $($Latest.URL64)"
           "(?i)(Get-RemoteChecksum).*"        = "`${1} $($Latest.URL64)"
@@ -15,19 +18,18 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page = Get-GitHubLatestReleaseLinks -User "FrodeSolheim" -Repository "fs-uae-launcher"
     
     $regex = '.zip$'
-    $url = $download_page.links | ? href -match $regex | select -First 1 -Skip 1 -expand href
-    
-    $version = $url -split '_' | select -Last 1 -Skip 2
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href | % { 'https://github.com' + $_ }
 
+    $version  = $url -split '_' | select -First 1 -Skip 1
+    
     @{
-      Version = $version
-      URL64   = $url
+        Version = $version
+        URL64 = $url
+        ReleaseNotes = "https://github.com/FrodeSolheim/fs-uae-launcher/releases/tag/v${version}"
     }
 }
 
-update -ChecksumFor None
+update -ChecksumFor none
