4579630f9847374905a60e1e3a5d013c6814eee5
jtcmedia <jason.cumiskey@gmail.com>
2025-12-17 12:51:03 -0800

    fix d/l links

--- zap/tools/chocolateyinstall.ps1
+++ zap/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = 'zap'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32          = 'https://github.com/zaproxy/zaproxy/releases/download/v2.16.1/ZAP_2_17_0_windows-x32.exe'
-$url64          = 'https://github.com/zaproxy/zaproxy/releases/download/v2.16.1/ZAP_2_17_0_windows.exe'
+$url32          = 'https://github.com/zaproxy/zaproxy/releases/download/v2.17.0/ZAP_2_17_0_windows-x32.exe'
+$url64          = 'https://github.com/zaproxy/zaproxy/releases/download/v2.17.0/ZAP_2_17_0_windows.exe'
 $checksum32     = '125fc89c73d440141ce859109e23258700b863b94475d5461926ec757d99e94a'
 $checksum64     = 'ebdaf6f00ffd9c21891d29360196e13a14091f84dde2bfa1e0b61213a93bc5ca'
 $pf             = ''
