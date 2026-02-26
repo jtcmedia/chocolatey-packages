b2a40808fe50370a31e0aca2e60d792f26a61509
jtcmedia <jtcmedia+github@gmail.com>
2026-02-26 04:04:40 +0000

    AU: 2 updated - awscli cypress [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/090b4b4c7f21bf8247dcfc5efdd62a1711deab26

--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://cdn.cypress.io/desktop/15.10.0/win32-x64/cypress.zip'
-$checksum64 = '1d674e6681e32b11a98366e65e5c50a834f2bfd5f30950bedb99141de0ffcd21'
+$url64      = 'https://cdn.cypress.io/desktop/15.11.0/win32-x64/cypress.zip'
+$checksum64 = 'bcb5a381a93c010b40278764230834aa6ee77dbed906cca7c13d5645f0df2528'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
