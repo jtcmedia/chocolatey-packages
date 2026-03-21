31b547f0b08222b68486bfb51698e8cebe1b346e
jtcmedia <jtcmedia+github@gmail.com>
2026-03-21 04:00:25 +0000

    AU: 1 updated - awscli [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/1d6f9b80a30fd1dd46d5a256cdd0f33154a4539e

--- awscli-v2/tools/chocolateyinstall.ps1
+++ awscli-v2/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.34.13.msi'
-$checksum64 = '97c09010012390259b7ec41fe5a65a7cfe6c1daab0fbfe8edae2121f84281c07'
+$url64      = 'https://awscli.amazonaws.com/AWSCLIV2-2.34.14.msi'
+$checksum64 = 'a19ca6a7ffb34b412bb4a8fa62bd3b6c4f3934bef717a3045612c1feaa5576e7'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
