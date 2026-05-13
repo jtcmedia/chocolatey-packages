b99ef06f647a68ed3d54460f5af21a2f8d7d1933
jtcmedia <jtcmedia+github@gmail.com>
2026-05-13 04:35:30 +0000

    AU: 2 updated - argocd-cli cypress [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/675073c49807e08cebd929479ef3fd46a1f69ebd

--- cypress/tools/chocolateyinstall.ps1
+++ cypress/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
-$url64      = 'https://cdn.cypress.io/desktop/15.14.2/win32-x64/cypress.zip'
-$checksum64 = 'ae1285a906676abcdb8b1e36ebf807ea54dba43e2e911a79f1380a508f266f12'
+$url64      = 'https://cdn.cypress.io/desktop/15.15.0/win32-x64/cypress.zip'
+$checksum64 = '19e619807aa24ec1acef65147de70cce2c71aa134442cd76123885ccf1ab3280'
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
