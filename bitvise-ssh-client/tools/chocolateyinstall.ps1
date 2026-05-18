1a13afb871d1cdad12f474ffe1fee93e20399af7
jtcmedia <jtcmedia+github@gmail.com>
2026-05-18 04:46:38 +0000

    AU: 1 updated - bitvise-ssh-client [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/941972ff90154871a7385c52f683e3292fd0f9b5

--- bitvise-ssh-client/tools/chocolateyinstall.ps1
+++ bitvise-ssh-client/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName= 'bitvise-ssh-client'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://dl.bitvise.com/BvSshClient-Inst.exe'
-$checksum32 = 'b903d4be4226ce79083b635b9ddb565932a843cfabaecaf0a1bf42afdcd855e8'
+$checksum32 = '41575a4e6d56d468506d2303ace7125dd42ca57d3ca25c4da0eacde79df0d82d'
 
 $packageArgs = @{
   packageName   = $packageName
