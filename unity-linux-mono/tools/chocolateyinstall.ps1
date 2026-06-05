f813689fb6d98e0900d274d2adf67bb04241fa88
jtcmedia <jtcmedia+github@gmail.com>
2026-06-05 14:37:33 +0000

    AU: 4 updated - arduino-cli unity-ios unity-linux-mono unity-mac-server [skip ci] https://gist.github.com/jtcmedia/8abc37a8c3b020ffdd9704a810331790/6f58d9532a921b5f61906094be0a29eb20ee491e

--- unity-linux-mono/tools/chocolateyinstall.ps1
+++ unity-linux-mono/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName    = $env:ChocolateyPackageName
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://download.unity3d.com/download_unity/feeafc12a938/TargetSupportInstaller/UnitySetup-Linux-Mono-Support-for-Editor-6000.4.10f1.exe'
-$checksum64     = 'e68220bf672242fb1d550d02c764f1c2f51e64cc9e93d1e2e74aa6c200155764'
+$url64          = 'https://download.unity3d.com/download_unity/cf0352b38e81/TargetSupportInstaller/UnitySetup-Linux-Mono-Support-for-Editor-6000.3.17f1.exe'
+$checksum64     = '943f55f447ae5ed4a7302036d0fa1a0a23111420621def67e394f3aa5eabb14f'
 
 $packageArgs = @{
   packageName    = $packageName
